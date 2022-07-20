31d4daf673292236fcc21214934c6bc7980971df
kich555 <hyunjjang199582@gmail.com>
2022-07-20 18:44:34 +0900

    Create Sample page

--- kongbab/app/routes/index.jsx
+++ kongbab/app/routes/index.jsx
@@ -4,8 +4,490 @@ export default function Index() {
   return (
     <>
       <Nav />
-      <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <div
+        style={{
+          fontFamily: 'system-ui, sans-serif',
+          lineHeight: '1.4',
+          marginTop: '100px',
+        }}
+      >
         <h1>Welcome to Remix</h1>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        v
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/blog'
+              rel='noreferrer'
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target='_blank'
+              href='https://remix.run/tutorials/jokes'
+              rel='noreferrer'
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+              Remix Docs
+            </a>
+          </li>
+        </ul>
         <ul>
           <li>
             <a
