ffad0b43c267e052bee6e62c1b5cdabef8ae696a
kich555 <hyunjjang199582@gmail.com>
2022-07-22 15:44:15 +0900

    Change theme.js to theme.jsx

--- kongbab/app/constants/theme.js
+++
@@ -1,7 +0,0 @@
-const BASIC_THEME = {
-    fontFamily: 'Pretendard',
-    colors: {carrot: "#FF4500"},
-    primaryColor: 'carrot',
-}
-
-export default BASIC_THEME
---
+++ kongbab/app/constants/theme.jsx
@@ -0,0 +1,12 @@
+const BASIC_THEME = {
+  fontFamily: 'Pretendard',
+  colors: {
+    carrot: '#FF4500',
+    dark: ['#040000', '#0A1C2D'],
+    blue: ['#364B60', '#B8C8D5', '#EDF2FB'],
+    // gray: ['#F2F4F7'],
+  },
+  primaryColor: 'carrot',
+};
+
+export default BASIC_THEME;
