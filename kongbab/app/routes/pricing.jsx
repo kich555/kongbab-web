4b758a58e246f3b896e7ad19e7608268a507f50c
kich555 <hyunjjang199582@gmail.com>
2022-07-24 03:54:38 +0900

    Creating process page

--- kongbab/app/routes/pricing.jsx
+++ kongbab/app/routes/pricing.jsx
@@ -1,13 +1,16 @@
-import PriceInfoContainer from 'components/page/Pricing/PriceInfoList';
+import FeatureList from 'components/page/Pricing/FeatureList';
+import PriceInfoList from 'components/page/Pricing/PriceInfoList';
 import Title from 'components/Title';
 import PRICING_CONSTANTS from '~/constants/page/Pricing';
 
 export default function pricing(params) {
-  const { RATE_PLANS } = PRICING_CONSTANTS;
+  const { RATE_PLANS, FEATURES } = PRICING_CONSTANTS;
   return (
     <>
       <Title title='요금 안내' />
-      <PriceInfoContainer plans={RATE_PLANS} />
+      <PriceInfoList plans={RATE_PLANS} />
+      <Title title='요금 안내' style={{ marginTop: '200px' }} />
+      <FeatureList features={FEATURES} />
     </>
   );
 }
