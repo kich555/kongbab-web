694c0a38a1644e6e80836044bbe17bcf5285d7cc
kich555 <hyunjjang199582@gmail.com>
2022-07-24 02:32:12 +0900

    Creating pricing page

--- kongbab/app/routes/pricing.jsx
+++ kongbab/app/routes/pricing.jsx
@@ -1,3 +1,13 @@
+import PriceInfoContainer from 'components/page/Pricing/PriceInfoList';
+import Title from 'components/Title';
+import PRICING_CONSTANTS from '~/constants/page/Pricing';
+
 export default function pricing(params) {
-  return <div>heoo</div>;
+  const { RATE_PLANS } = PRICING_CONSTANTS;
+  return (
+    <>
+      <Title title='요금 안내' />
+      <PriceInfoContainer plans={RATE_PLANS} />
+    </>
+  );
 }
