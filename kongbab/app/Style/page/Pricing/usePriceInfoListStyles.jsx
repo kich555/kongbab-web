2094b54dba8ea167877a8b1ef46cd40153e4e8f4
kich555 <hyunjjang199582@gmail.com>
2022-07-24 02:31:45 +0900

    Create PriceInfo & PriceInfoList components Styles

---
+++ kongbab/app/Style/page/Pricing/usePriceInfoListStyles.jsx
@@ -0,0 +1,39 @@
+import { createStyles } from '@mantine/core';
+
+const usePriceInfoListStyles = createStyles((theme) => ({
+  wrapper: {
+    display: 'flex',
+    marginTop: '80px',
+  },
+  list: {
+    borderRadius: '24px',
+    overflow: 'hidden',
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+  another: {
+    width: '252px',
+    marginLeft: '6px',
+  },
+  box: {
+    display: 'flex',
+  },
+  title: {
+    paddingTop: '14px',
+    paddingBottom: '15px',
+  },
+  listTitle: {
+    background: 'linear-gradient(90deg, #FF4545 0%, #FF9D66 100%)',
+  },
+  anotherTitle: {
+    background: theme.colors.blue[0],
+  },
+  anotherDesc: {
+    marginTop: '24px',
+    padding: '0 16px 0 16px',
+    textAlign: 'center',
+    lineHeight: '150%',
+  },
+}));
+
+export default usePriceInfoListStyles;
---
+++ kongbab/app/Style/page/Pricing/usePriceInfoStyles.jsx
@@ -0,0 +1,30 @@
+import { createStyles } from '@mantine/core';
+import common from '~/constants/commonStyle';
+
+const { ...marginTop20 } = common;
+
+const usePriceInfoStyles = createStyles((theme) => ({
+  box: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '246px',
+    padding: '30px 14px 25px 14px',
+    textAlign: 'center',
+    '&:nth-child(-n+2)': {
+      borderRight: '1px solid #EDF0F4',
+    },
+  },
+  list: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    gap: '15px',
+    textAlign: 'left',
+  },
+  listItem: {
+    height: '16px',
+  },
+  ...marginTop20,
+}));
+
+export default usePriceInfoStyles;
