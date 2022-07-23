52e9f9f3e4c18b832685b98bdfa9c8eff5cee142
kich555 <hyunjjang199582@gmail.com>
2022-07-24 02:33:09 +0900

    Update Mantine Theme fontSizes

--- kongbab/app/constants/theme.jsx
+++ kongbab/app/constants/theme.jsx
@@ -1,5 +1,14 @@
 const BASIC_THEME = {
   fontFamily: 'Pretendard',
+  lineHeight: '100%',
+  black: '#0A1C2D',
+  fontSizes: {
+    xs: '12px',
+    sm: '14px',
+    md: '16px',
+    lg: '24px',
+    xl: '36px',
+  },
   colors: {
     carrot: '#FF4500',
     dark: ['#040000', '#0A1C2D'],
