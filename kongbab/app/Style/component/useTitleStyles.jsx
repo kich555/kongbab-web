63a0132d81f3736b03ff2053cf1c6a3f342d8231
kich555 <hyunjjang199582@gmail.com>
2022-07-24 02:34:15 +0900

    Create Title style

---
+++ kongbab/app/Style/component/useTitleStyles.jsx
@@ -0,0 +1,13 @@
+import { createStyles } from '@mantine/core';
+
+const useTitleStyles = createStyles((theme) => ({
+  section: {
+    marginTop: '120px',
+    fontWeight: 700,
+    fontSize: '36px',
+    lineHeight: '100%',
+    textAlign: 'center',
+  },
+}));
+
+export default useTitleStyles;
