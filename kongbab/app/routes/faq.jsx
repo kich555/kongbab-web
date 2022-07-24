e060128af9fde14a54969780b12269c1739abb2b
kich555 <hyunjjang199582@gmail.com>
2022-07-24 20:40:12 +0900

    Complete faq page

--- kongbab/app/routes/faq.jsx
+++ kongbab/app/routes/faq.jsx
@@ -2,23 +2,26 @@ import { Container } from '@mantine/core';
 import Title from 'components/Title';
 import AccordionSection from 'components/AccordionSection';
 import FAQ_CONSTANTS from '../constants/page/faqPageData';
+import ConsultationSection from 'components/page/FAQ/ConsultationSection';
 
 export default function faq(params) {
   const { SERVICE_FAQ, PRICE_FAQ } = FAQ_CONSTANTS;
   return (
     <>
-      <section>
+      <section style={{ padding: '0 26px 0 26px' }}>
         <Container size='sm'>
           <Title title='자주 묻는 질문' style={{ marginTop: '120px' }} />
           <AccordionSection questions={SERVICE_FAQ} />
         </Container>
       </section>
-      <section>
+      <section style={{ padding: '0 26px 0 26px' }}>
         <Container size='sm' style={{ marginTop: '150px' }}>
           <AccordionSection questions={PRICE_FAQ} />
         </Container>
       </section>
-      <section>tes</section>
+      <section style={{ padding: '0 26px 0 26px' }}>
+        <ConsultationSection />
+      </section>
     </>
   );
 }
