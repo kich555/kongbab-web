5e9f0b7a51258261d000088758ad918550cee6ce
kich555 <hyunjjang199582@gmail.com>
2022-07-24 19:26:05 +0900

    Change Pricing Constant data Directory & file name

--- kongbab/app/routes/pricing.jsx
+++ kongbab/app/routes/pricing.jsx
@@ -2,7 +2,7 @@ import FeatureList from 'components/page/Pricing/FeatureList';
 import PriceInfoList from 'components/page/Pricing/PriceInfoList';
 import EventSection from 'components/page/Pricing/EventSection';
 import FeaturedFAQSection from 'components/page/Pricing/FeaturedFAQSection';
-import PRICING_CONSTANTS from '~/constants/page/Pricing';
+import PRICING_CONSTANTS from '~/constants/page/pricingPageData';
 
 export default function pricing(params) {
   const { RATE_PLANS, FEATURES, FEATURED_FAQ } = PRICING_CONSTANTS;
