82f7d478ff991e26d37a207b1a2a44b141112bba
kich555 <hyunjjang199582@gmail.com>
2022-07-24 19:20:20 +0900

    Seperate AccordionSection to Common Component

--- kongbab/app/Style/page/Pricing/useFeaturedFAQStyles.jsx
+++ kongbab/app/Style/page/Pricing/useFeaturedFAQStyles.jsx
@@ -4,22 +4,6 @@ const useFeaturedFAQStyles = createStyles((theme) => ({
   warpper: {
     padding: '214px 0 200px 0 ',
   },
-  root: {
-    marginTop: '25px',
-  },
-  content: {
-    padding: '0 0 38px 0',
-    borderBottom: '1px solid #EDF0F4',
-  },
-  contentInner: { padding: '0px' },
-  item: { borderBottom: '1px solid #EDF0F4' },
-  itemOpened: { borderBottom: 'none' },
-  control: {
-    padding: '38px 0 38px 0',
-    '&:hover': {
-      backgroundColor: 'transparent',
-    },
-  },
   button: {
     width: '140px',
     height: '44px',
--- kongbab/components/page/Pricing/FeaturedFAQSection.jsx
+++ kongbab/components/page/Pricing/FeaturedFAQSection.jsx
@@ -1,71 +1,19 @@
-import { Accordion, Container, Text, Center, Button } from '@mantine/core';
+import { Container, Text, Center, Button } from '@mantine/core';
+import AccordionSection from 'components/AccordionSection';
 import Title from 'components/Title';
-import ArrowRight from '~/asset/icon/ArrowRight';
 import useFeaturedFAQStyles from '~/Style/page/Pricing/useFeaturedFAQStyles';
 
 export default function FeaturedFAQSection({ questions }) {
-  const { classes, theme } = useFeaturedFAQStyles();
-  const { primaryColor, colors } = theme;
-  const {
-    warpper,
-    root,
-    control,
-    content,
-    contentInner,
-    item,
-    itemOpened,
-    button,
-  } = classes;
-  const items = questions.map((question) => (
-    <Accordion.Item
-      key={question.label}
-      label={
-        <Text size='lg' weight={500}>
-          {question.label}
-        </Text>
-      }
-    >
-      <Text
-        color={colors['blue'][0]}
-        style={{ lineHeight: '150%', fontSize: '20px' }}
-      >
-        {question.desc}
-      </Text>
-      {/* <Divider my='sm' color={colors['gray'][1]} /> */}
-    </Accordion.Item>
-  ));
+  const { classes } = useFeaturedFAQStyles();
+  const { warpper, button } = classes;
+
   return (
     <Container size='sm' className={warpper}>
       <Title title='자주 묻는 질문' />
-      <Text
-        weight={700}
-        color={colors[primaryColor]}
-        style={{ marginTop: '100px', fontSize: '20px' }}
-      >
-        요금
-      </Text>
-      <Accordion
-        icon={<ArrowRight size={14} />}
-        iconPosition='right'
-        multiple
-        className={root}
-        classNames={{
-          item,
-          itemOpened,
-          control,
-          content,
-          contentInner,
-        }}
-      >
-        {items}
-      </Accordion>
+      <AccordionSection questions={questions} />
       <Center>
         <Button
-          // sx={(theme) => ({
-          //   backgroundColor: theme.colors.dark[1],
-          // })}
           className={button}
-          // variant='filled'
           radius={8}
           onClick={() => console.log('test')}
         >
