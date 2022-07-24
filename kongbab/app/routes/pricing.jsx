993afac1a540aad8bbcb1f4cb98cc1480eff9deb
kich555 <hyunjjang199582@gmail.com>
2022-07-24 12:45:56 +0900

    Apply EventSection on pricing page

--- kongbab/app/routes/pricing.jsx
+++ kongbab/app/routes/pricing.jsx
@@ -1,14 +1,20 @@
 import FeatureList from 'components/page/Pricing/FeatureList';
 import PriceInfoList from 'components/page/Pricing/PriceInfoList';
+import EventSection from 'components/page/Pricing/EventSection';
 import PRICING_CONSTANTS from '~/constants/page/Pricing';
 
 export default function pricing(params) {
   const { RATE_PLANS, FEATURES } = PRICING_CONSTANTS;
 
   return (
-    <section style={{ backgroundColor: '#F2F4F7' }}>
-      <PriceInfoList plans={RATE_PLANS} />
-      <FeatureList features={FEATURES} />
-    </section>
+    <>
+      <section style={{ backgroundColor: '#F2F4F7' }}>
+        <PriceInfoList plans={RATE_PLANS} />
+        <FeatureList features={FEATURES} />
+      </section>
+      <section>
+        <EventSection />
+      </section>
+    </>
   );
 }
