0886a59ea376989d7b98172a398ae527262542a4
kich555 <hyunjjang199582@gmail.com>
2022-07-26 02:34:47 +0900

    Apply ConsultationCard component on FAQConsultationSection component

---
+++ kongbab/components/page/FAQ/FAQConsultationSection.jsx
@@ -0,0 +1,21 @@
+import { Text, useMantineTheme } from '@mantine/core';
+import asset from '~/asset/image/FAQ_image.png';
+import ConsultationCard from '../../ConsultationCard';
+
+export default function FAQConsultationSection(params) {
+  const theme = useMantineTheme();
+  const { white } = theme;
+  return (
+    <ConsultationCard asset={asset}>
+      <Text
+        style={{ textAlign: 'center', fontSize: '30px', lineHeight: '150%' }}
+        color={white}
+        weight={700}
+      >
+        아직 망설여지나요?
+        <br />
+        상담은 언제나 환영합니다.
+      </Text>
+    </ConsultationCard>
+  );
+}
