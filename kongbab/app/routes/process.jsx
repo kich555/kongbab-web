b4a1dae4156ffc955c0e54dba664a06420dbeb4f
kich555 <hyunjjang199582@gmail.com>
2022-07-27 00:28:11 +0900

    Creating process page

--- kongbab/app/routes/process.jsx
+++ kongbab/app/routes/process.jsx
@@ -1,9 +1,17 @@
+import { Container, Divider, Space } from '@mantine/core';
 import ProcessConsultationSection from 'components/page/Process/ProcessConsultationSection';
+import LitigationProcedureTitle from 'components/page/Process/LitigationProcedureTitle';
 import LitigationProcedure from 'components/page/Process/LitigationProcedure';
 
 export default function process() {
   return (
     <section>
+      <LitigationProcedureTitle />
+      <Container>
+        <Space h={40} />
+        <Divider size='xs' color='#EDF0F4' />
+      </Container>
+      <Space h={80} />
       <LitigationProcedure />
       <ProcessConsultationSection />
     </section>
