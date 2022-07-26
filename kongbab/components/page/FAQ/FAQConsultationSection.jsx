0cf404de0f47558146cd9d82ffe7eea72c499314
kich555 <hyunjjang199582@gmail.com>
2022-07-26 19:32:17 +0900

    Apply Responsive on FAQConsultationSection

--- kongbab/components/page/FAQ/FAQConsultationSection.jsx
+++ kongbab/components/page/FAQ/FAQConsultationSection.jsx
@@ -1,4 +1,4 @@
-import { Text, useMantineTheme } from '@mantine/core';
+import { Text, MediaQuery, useMantineTheme } from '@mantine/core';
 import asset from '~/asset/image/FAQ_image.png';
 import ConsultationCard from '../../ConsultationCard';
 
@@ -6,16 +6,24 @@ export default function FAQConsultationSection(params) {
   const theme = useMantineTheme();
   const { white } = theme;
   return (
-    <ConsultationCard asset={asset}>
-      <Text
-        style={{ textAlign: 'center', fontSize: '30px', lineHeight: '150%' }}
-        color={white}
-        weight={700}
-      >
-        아직 망설여지나요?
-        <br />
-        상담은 언제나 환영합니다.
-      </Text>
-    </ConsultationCard>
+    <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
+      <div>
+        <ConsultationCard asset={asset}>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: '30px',
+              lineHeight: '150%',
+            }}
+            color={white}
+            weight={700}
+          >
+            아직 망설여지나요?
+            <br />
+            상담은 언제나 환영합니다.
+          </Text>
+        </ConsultationCard>
+      </div>
+    </MediaQuery>
   );
 }
