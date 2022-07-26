08aa47efbf744d691cd0acb8fa53c1e9aa17c3c6
kich555 <hyunjjang199582@gmail.com>
2022-07-26 19:46:34 +0900

    Refactoring AccordionSection mediaQuery

--- kongbab/components/AccordionSection.jsx
+++ kongbab/components/AccordionSection.jsx
@@ -1,42 +1,52 @@
-import { Accordion, Text } from '@mantine/core';
+import { Accordion, MediaQuery, Text } from '@mantine/core';
 import ArrowRight from '~/asset/icon/ArrowRight';
-import useAccordianStyles from '../app/Style/component/useAccordianStyles';
+import useAccordianStyles from '~/Style/component/useAccordianStyles';
 
 export default function AccordionSection({ questions, style }) {
   const { classes, theme } = useAccordianStyles();
   const { colors, primaryColor } = theme;
-  const { root, control, content, contentInner, item, itemOpened } = classes;
+  const { control, content, contentInner, item, itemOpened } = classes;
+
   const items = questions.map((question) => (
     <Accordion.Item
       key={question.label}
       label={
-        <Text size='lg' weight={500}>
-          {question.label}
-        </Text>
+        <MediaQuery smallerThan='sm' styles={{ fontSize: theme.fontSizes.md }}>
+          <Text size='lg' weight={500}>
+            {question.label}
+          </Text>
+        </MediaQuery>
       }
     >
-      <Text
-        color={colors['blue'][0]}
-        style={{ lineHeight: '150%', fontSize: '20px' }}
-      >
-        {question.desc}
-      </Text>
+      <MediaQuery smallerThan='sm' styles={{ fontSize: theme.fontSizes.md }}>
+        <Text
+          color={colors['blue'][0]}
+          style={{ fontSize: '20px', lineHeight: '150%' }}
+        >
+          {question.desc}
+        </Text>
+      </MediaQuery>
     </Accordion.Item>
   ));
   return (
     <>
-      <Text
-        weight={700}
-        color={colors[primaryColor]}
-        style={{ marginTop: '100px', fontSize: '20px' }}
+      <MediaQuery
+        smallerThan='sm'
+        styles={{ margin: '60px 0 15px 0', fontSize: theme.fontSizes.md }}
       >
-        요금
-      </Text>
+        <Text
+          weight={700}
+          color={colors[primaryColor]}
+          style={{ margin: '100px 0 25px 0', fontSize: '20px' }}
+        >
+          요금
+        </Text>
+      </MediaQuery>
+
       <Accordion
         icon={<ArrowRight size={14} />}
         iconPosition='right'
         multiple
-        className={root}
         classNames={{
           item,
           itemOpened,
