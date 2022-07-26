e5820f7281d7de5f3f99e3b7fd30a124bc8d8189
kich555 <hyunjjang199582@gmail.com>
2022-07-26 20:01:33 +0900

    Refactoring FAQ page

--- kongbab/app/routes/faq.jsx
+++ kongbab/app/routes/faq.jsx
@@ -1,25 +1,13 @@
-import { Container } from '@mantine/core';
-import Title from 'components/Title';
-import AccordionSection from 'components/AccordionSection';
-import FAQ_CONSTANTS from '~/constants/page/faqPageData';
 import FAQConsultationSection from 'components/page/FAQ/FAQConsultationSection';
+import FAQAccordionSection from 'components/page/FAQ/FAQAccordionSection';
 
-export default function faq(params) {
-  const { SERVICE_FAQ, PRICE_FAQ } = FAQ_CONSTANTS;
+export default function faq() {
   return (
     <>
-      <section style={{ padding: '0 26px 0 26px' }}>
-        <Container size='sm'>
-          <Title title='자주 묻는 질문' style={{ marginTop: '120px' }} />
-          <AccordionSection questions={SERVICE_FAQ} />
-        </Container>
+      <section>
+        <FAQAccordionSection />
       </section>
-      <section style={{ padding: '0 26px 0 26px' }}>
-        <Container size='sm' style={{ marginTop: '150px' }}>
-          <AccordionSection questions={PRICE_FAQ} />
-        </Container>
-      </section>
-      <section style={{ padding: '0 26px 0 26px' }}>
+      <section>
         <FAQConsultationSection />
       </section>
     </>
