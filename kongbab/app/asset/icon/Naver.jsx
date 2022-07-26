1920f41b69cb12141d508baf451e9e3d24b0e144
kich555 <hyunjjang199582@gmail.com>
2022-07-27 00:35:12 +0900

    Add SNS Icon

--- kongbab/app/asset/icon/Naver.jsx
+++ kongbab/app/asset/icon/Naver.jsx
@@ -6,7 +6,7 @@ export default function Naver() {
       viewBox='0 0 40 40'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
-      xmlns:xlink='http://www.w3.org/1999/xlink'
+      // xmlns:xlink='http://www.w3.org/1999/xlink'
     >
       <rect width='40' height='40' fill='url(#pattern0)' />
       <defs>
