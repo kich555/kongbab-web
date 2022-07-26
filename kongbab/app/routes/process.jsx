7b9e8a10f6b7a6f1ae21820e5d1039a30e253099
kich555 <hyunjjang199582@gmail.com>
2022-07-26 20:18:07 +0900

    Creating process page

--- kongbab/app/routes/process.jsx
+++ kongbab/app/routes/process.jsx
@@ -1,8 +1,10 @@
 import ProcessConsultationSection from 'components/page/Process/ProcessConsultationSection';
+import LitigationProcedure from 'components/page/Process/LitigationProcedure';
 
 export default function process() {
   return (
     <section>
+      <LitigationProcedure />
       <ProcessConsultationSection />
     </section>
   );
