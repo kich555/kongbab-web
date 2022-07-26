5f8ec14d677629b3cca68e39878dc6db66223523
kich555 <hyunjjang199582@gmail.com>
2022-07-26 20:03:30 +0900

    Refactoring AccordionStyle

--- kongbab/app/Style/component/useAccordianStyles.jsx
+++ kongbab/app/Style/component/useAccordianStyles.jsx
@@ -1,13 +1,13 @@
 import { createStyles } from '@mantine/core';
 
 const useAccordianStyles = createStyles((theme) => ({
-  root: {
-    marginTop: '25px',
-  },
   item: { borderBottom: '1px solid #EDF0F4', borderTop: '1px solid #EDF0F4' },
   itemOpened: { borderBottom: 'none' },
   control: {
-    padding: '38px 0 38px 0',
+    padding: '38px 0',
+    [theme.fn.smallerThan('sm')]: {
+      padding: '25px 0',
+    },
     '&:hover': {
       backgroundColor: 'transparent',
     },
