d800589e55235df792781460a0e4c426b891c668
kich555 <hyunjjang199582@gmail.com>
2022-07-28 03:06:38 +0900

    Apply Mobile Responsive on ProcessConsultationSection component

--- kongbab/components/page/Process/ProcessConsultationSection.jsx
+++ kongbab/components/page/Process/ProcessConsultationSection.jsx
@@ -1,20 +1,27 @@
 import { Space, Text, useMantineTheme } from '@mantine/core';
 import asset from '~/asset/image/Process_footer_image.png';
-import ConsultationCard from '../../ConsultationCard';
+import useResponsive from '~/hooks/useResponsive';
+import ConsultationCard from 'components/ConsultationCard';
 
 export default function ProcessConsultationSection() {
   const theme = useMantineTheme();
   const { white } = theme;
+  const mobile = useResponsive();
   return (
     <ConsultationCard asset={asset}>
-      <Text color={white} size='xl' weight={700}>
+      <Text color={white} size={mobile ? 'lg' : 'xl'} weight={700}>
         ⚡⚡⚡
       </Text>
       <Space h='lg' />
       <Text
-        style={{ textAlign: 'center', fontSize: '30px', lineHeight: '150%' }}
+        align='center'
         color={white}
         weight={700}
+        style={
+          mobile
+            ? { fontSize: '20px', lineHeight: '150%' }
+            : { fontSize: '30px', lineHeight: '150%' }
+        }
       >
         명도소송은 스피드가 생명
         <br />
