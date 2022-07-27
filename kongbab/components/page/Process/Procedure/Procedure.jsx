8b60c023cacebb9aa27c17aa3eef02caa99d52a1
kich555 <hyunjjang199582@gmail.com>
2022-07-27 20:02:58 +0900

    Refactoring Procedure component

--- kongbab/components/page/Process/Procedure/Procedure.jsx
+++ kongbab/components/page/Process/Procedure/Procedure.jsx
@@ -1,26 +1,32 @@
-import { Text, Space, Group } from '@mantine/core';
+import { Text, Space, Group, Badge, Center } from '@mantine/core';
 import useProcedureStyles from '~/Style/page/Process/useProcedureStyles';
 import Sticker from './Sticker/Sticker';
 import StickerList from './Sticker/StickerList';
 
 export default function Procedure({ procedureForm }) {
-  const { title, desc, condition, procedureList } = procedureForm;
+  const { title, desc, condition, procedureList, badgeContent } = procedureForm;
   const { classes, theme } = useProcedureStyles();
-  const { lineHeight150 } = classes;
+  const { lineHeight150, badge, badgeWrapper } = classes;
   const { colors } = theme;
   return (
     <>
-      <Text size='lg' weight={700}>
-        {title}
-      </Text>
+      <div className={badgeWrapper}>
+        <Text size='lg' component='span' weight={700}>
+          {title}
+        </Text>
+        {badgeContent && <span className={badge}>{badgeContent}</span>}
+      </div>
       <Space h={24} />
       <Text size='sm' className={lineHeight150}>
         {desc}
       </Text>
       {condition && (
-        <Text size='sm' weight={700} color={colors.blue[0]}>
-          {condition}
-        </Text>
+        <>
+          <Space h={15} />
+          <Text size='sm' weight={700} color={colors.blue[0]}>
+            {condition}
+          </Text>
+        </>
       )}
       <Space h={29} />
       <Group position='center' spacing={24}>
