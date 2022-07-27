c6efb4329d3aad2ffbcfd2bb37b3e2402e439ba4
kich555 <hyunjjang199582@gmail.com>
2022-07-27 20:04:17 +0900

    Add some classes on Procedure styles

--- kongbab/app/Style/page/Process/useProcedureStyles.jsx
+++ kongbab/app/Style/page/Process/useProcedureStyles.jsx
@@ -7,14 +7,30 @@ const useProcedureStyles = createStyles((theme) => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   centerColumn: {
     flexDirection: 'column',
     maxWidth: '428px',
     textAlign: 'center',
   },
-
+  badgeWrapper: {
+    position: 'relative',
+  },
+  badge: {
+    display: 'inline',
+    position: 'absolute',
+    top: '-4px',
+    marginLeft: '12px',
+    padding: '7px',
+    borderRadius: '6px',
+    fontSize: theme.fontSizes.xs,
+    fontWeight: 600,
+    color: theme.colors.magenta[0],
+    backgroundColor: theme.colors.magenta[1],
+  },
   lineHeight150: { lineHeight: '150%' },
   font18: { fontSize: '18px' },
+  font11: { fontSize: '11px' },
   pointer: { cursor: 'pointer' },
 }));
 
