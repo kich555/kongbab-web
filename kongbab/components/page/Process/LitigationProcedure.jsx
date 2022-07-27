58ab70a971ab2f6dc23c8d8b8cf22745c0ebb327
kich555 <hyunjjang199582@gmail.com>
2022-07-27 19:14:42 +0900

    Creating LitigationProcedure component

--- kongbab/components/page/Process/LitigationProcedure.jsx
+++ kongbab/components/page/Process/LitigationProcedure.jsx
@@ -1,108 +1,69 @@
-import {
-  Container,
-  Image,
-  Text,
-  Center,
-  Space,
-  Group,
-  Divider,
-} from '@mantine/core';
-import asset from '~/asset/image/Process_image.png';
-import useProcedureStyles from '../../../app/Style/page/Process/useProcedureStyles';
-import Sticker from './Sticker';
+import { Container, Center, Space, Divider } from '@mantine/core';
+import Arrow from '~/asset/icon/procedure/Arrow';
+import BranchArrow from '~/asset/icon/procedure/BranchArrow';
+import useProcedureStyles from '~/Style/page/Process/useProcedureStyles';
+import ComplaintSubmission from './Procedure/UnauthorizedPossession/ComplaintSubmission';
+import Consultation from './Procedure/Consultation';
+import ProhibitionEstateTransfer from './Procedure/ProhibiteTransferOwnership/ProhibitionEstateTransfer';
+import Procedure from './Procedure/Procedure';
+import PROCESS_CONSTANTS from '~/constants/page/processPageData';
 
 export default function LitigationProcedure(params) {
-  const { classes, theme } = useProcedureStyles();
+  const { classes } = useProcedureStyles();
   const { wrapper } = classes;
-  const { colors, black } = theme;
+  const { PROCEDURE } = PROCESS_CONSTANTS;
+  const { CONSULTATION, PROHIBITE_TRANSFER_OWNERSHIP, DETINUE } = PROCEDURE;
+  const {
+    PROHIBITION_ESTATE_TRANSFER,
+    MORTAGE_ORDER,
+    SENTENCE_INJUNCTION,
+    EXCUTE_INJUNCTION,
+  } = PROHIBITE_TRANSFER_OWNERSHIP;
+  const {
+    LODGING_COMPLAINT,
+    SERVE_TO_DEFENDANT,
+    CHECK_DEFENDANT_ANSWER,
+    NOTICE_ARGUMENT_DATE,
+    SENTENCE_JUDGEMENT,
+    EXECUTE_JUDGEMENT,
+  } = DETINUE;
   return (
-    <Container size='sm' className={wrapper}>
-      <Container size='xs'>
-        <Center style={{ flexDirection: 'column', maxWidth: '428px' }}>
-          <Text size='lg' weight={700}>
-            상담 및 수임 의뢰
-          </Text>
-          <Space h={24} />
-          <Text size='sm' style={{ lineHeight: '150%', textAlign: 'center' }}>
-            카카오톡을 통해 상담 후 소송 진행이 가능한 경우 수임 계약을
-            맺습니다.
-            <br />
-            카톡 상담신청
-          </Text>
-          <Space h={50} />
-          <Group position='center' spacing={24}>
-            <Sticker>카카오톡 상담</Sticker>
-            <Sticker icon>소송 진행 방법 제시</Sticker>
-            <Sticker>수임료 결제, 위임장/계약서 전자서명</Sticker>
-          </Group>
-          <Space h={30} />
-        </Center>
-      </Container>
+    <Container size='sm' px={0} className={wrapper}>
+      <Consultation procedureForm={CONSULTATION} />
+
       <Center>
-        <div className={wrapper}>
-          <Divider
-            style={{ margin: 'auto' }}
-            color={colors.blue[4]}
-            variant='dotted'
-            orientation='vertical'
-            sx={{ height: '60px' }}
-          />
-        </div>
+        <BranchArrow />
       </Center>
-      <Divider
-        style={{ margin: 'auto' }}
-        color={colors.blue[4]}
-        variant='dotted'
-        sx={{ width: '552px' }}
-      />
-      <Container size={552} px={0}>
-        <Center style={{ justifyContent: 'space-between' }}>
-          <Divider
-            style={{ marginRight: 'auto' }}
-            color={colors.blue[4]}
-            variant='dotted'
-            orientation='vertical'
-            sx={{ height: '60px' }}
-          />
-          <Divider
-            style={{ marginLeft: 'auto' }}
-            color={colors.blue[4]}
-            variant='dotted'
-            orientation='vertical'
-            sx={{ height: '60px' }}
-          />
-        </Center>
-        <Space h={50} />
-      </Container>
-      <Container size='sm'>
-        <Center style={{ justifyContent: 'space-between' }}>
+      <Space h={50} />
+      <Container size='sm' px={0}>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <Container
             size='xs'
             px={0}
-            style={{ flexGrow: 1, textAlign: 'center', margin: 0 }}
+            style={{ margin: 0, width: '100%', textAlign: 'center' }}
           >
-            <Text size='lg' weight={700}>
-              부동산점유이전금지 가처분 신청
-            </Text>
+            <Procedure procedureForm={PROHIBITION_ESTATE_TRANSFER} />
+            <Space h={30} />
+            <Arrow />
+            <Space h={45} />
+            <Procedure procedureForm={MORTAGE_ORDER} />
+            <Space h={30} />
+            <Arrow />
+            <Space h={45} />
           </Container>
           <Container
             size='xs'
             px={0}
-            style={{ flexGrow: 1, textAlign: 'center', margin: 0 }}
+            style={{ margin: 0, width: '100%', textAlign: 'center' }}
           >
-            <Text size='lg' weight={700}>
-              소장 제출
-            </Text>
+            {/* <ComplaintSubmission /> */}
           </Container>
-        </Center>
+        </div>
       </Container>
       <Container>
         <Space h={40} />
         <Divider size='xs' color='#EDF0F4' />
       </Container>
-      <Image src={asset} alt='test' />
-      {/* <Sticker>소송 진행 방법 제시</Sticker>
-      <Sticker icon>소송 진행 방법 제시</Sticker> */}
     </Container>
   );
 }
