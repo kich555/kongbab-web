a83df1c13d02faf9e477ea127acf8b52b4ae23df
kich555 <hyunjjang199582@gmail.com>
2022-07-27 22:03:46 +0900

    Refactoring FeaturedList

--- kongbab/app/Style/page/Pricing/useFeatureListStyles.jsx
+++ kongbab/app/Style/page/Pricing/useFeatureListStyles.jsx
@@ -1,20 +1,10 @@
 import { createStyles } from '@mantine/core';
 
 const useFeatureListStyles = createStyles((theme) => ({
-  warpper: {
-    padding: '200px 0 150px 0 ',
-  },
-  root: {
-    listStyle: 'none',
-    display: 'flex',
-    justifyContent: 'center',
-    flexWrap: 'wrap',
-    gap: '24px',
-    paddingTop: '100px',
-  },
   item: {
     overflow: 'hidden',
     maxWidth: '252px',
+    margin: 0,
     padding: '35px 35px 50px 35px',
     borderRadius: '20px',
     boxShadow: '0px 4px 10px rgba(54, 75, 96, 0.05)',
--- kongbab/components/page/Pricing/FeatureList.jsx
+++ kongbab/components/page/Pricing/FeatureList.jsx
@@ -1,24 +1,34 @@
-import { Container, List } from '@mantine/core';
-import FeatureInfo from './FeatureInfo';
+import { Container, Group, Text, Space } from '@mantine/core';
 import Title from 'components/Title';
 import useFeatureListStyles from '~/Style/page/Pricing/useFeatureListStyles';
 
 export default function FeatureList({ features }) {
   const { classes } = useFeatureListStyles();
-  const { warpper, root, item } = classes;
+  const { item } = classes;
+
   const items = features.map((feature, idx) => (
-    <FeatureInfo
-      key={feature.title}
-      index={idx}
-      color={feature.color}
-      title={feature.title}
-      desc={feature.desc}
-    />
+    <Container key={feature.title} className={item}>
+      <Text size='xl' color={feature.color} weight={700}>
+        {idx + 1}
+      </Text>
+      <Space h={30} />
+      <Text size='lg' weight={700}>
+        {feature.title}
+      </Text>
+      <Space h={19} />
+      <Text size='md' style={{ lineHeight: '150%' }}>
+        {feature.desc}
+      </Text>
+    </Container>
   ));
+
   return (
-    <Container className={warpper}>
-      <Title title='요금 안내' style={{ marginTop: '200px' }} />
-      <List classNames={{ root, item }}>{items}</List>
+    <Container px={0}>
+      <Space h={80} />
+      <Title title='요금 안내' />
+      <Space h={100} />
+      <Group spacing={24}>{items}</Group>
+      <Space h={150} />
     </Container>
   );
 }
