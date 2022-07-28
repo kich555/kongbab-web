31473d52b1c7259741666f17174a094cc6ebdbd0
kich555 <hyunjjang199582@gmail.com>
2022-07-28 17:30:48 +0900

    Apply Responsive on LitigationProcedure component

--- kongbab/components/page/Process/Procedure/LitigationProcedure.jsx
+++ kongbab/components/page/Process/Procedure/LitigationProcedure.jsx
@@ -1,74 +1,37 @@
-import { Container, Center, Space, Divider } from '@mantine/core';
-import Arrow from '~/asset/icon/procedure/Arrow';
+import { Container, Center, Space, Divider, MediaQuery } from '@mantine/core';
 import BranchArrow from '~/asset/icon/procedure/BranchArrow';
 import useProcedureStyles from '~/Style/page/Process/useProcedureStyles';
 import Consultation from './Consultation';
-import Procedure from './Procedure';
 import PROCESS_CONSTANTS from '~/constants/page/processPageData';
+import MobileProcedureList from './Mobile/MobileProcedureList';
+import useResponsive from '~/hooks/useResponsive';
+import ProcedureList from './Desktop/ProcedureList';
 
 export default function LitigationProcedure(params) {
   const { classes, theme } = useProcedureStyles();
   const { colors } = theme;
   const { wrapper } = classes;
   const { PROCEDURE } = PROCESS_CONSTANTS;
-  const { CONSULTATION, PROHIBITE_TRANSFER_OWNERSHIP, DETINUE } = PROCEDURE;
-  const {
-    PROHIBITION_ESTATE_TRANSFER,
-    MORTAGE_ORDER,
-    SENTENCE_INJUNCTION,
-    EXCUTE_INJUNCTION,
-  } = PROHIBITE_TRANSFER_OWNERSHIP;
-  const {
-    LODGING_COMPLAINT,
-    SERVE_TO_DEFENDANT,
-    CHECK_DEFENDANT_ANSWER,
-    NOTICE_ARGUMENT_DATE,
-    SENTENCE_JUDGEMENT,
-    EXECUTE_JUDGEMENT,
-  } = DETINUE;
+  const { CONSULTATION } = PROCEDURE;
+  const mobile = useResponsive();
   return (
     <Container size='sm' px={0} className={wrapper}>
-      <Space h={40} />
-      <Divider size='xs' color={colors.gray[1]} />
-      <Space h={80} />
-      <Consultation procedureForm={CONSULTATION} />
-      <Center>
-        <BranchArrow />
-      </Center>
-      <Space h={50} />
-      <Container size='sm' px={0}>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Container
-            size='xs'
-            px={0}
-            style={{ margin: 0, width: '100%', textAlign: 'center' }}
-          >
-            <Procedure procedureForm={PROHIBITION_ESTATE_TRANSFER} />
-            <Arrow />
-            <Procedure procedureForm={MORTAGE_ORDER} />
-            <Arrow />
-            <Procedure procedureForm={SENTENCE_INJUNCTION} />
-            <Arrow />
-            <Procedure procedureForm={EXCUTE_INJUNCTION} />
+      <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
+        <div>
+          <Space h={40} />
+          <Divider size='xs' color={colors.gray[1]} />
+          <Space h={80} />
+          <Container size='xs' px={0}>
+            <Consultation procedureForm={CONSULTATION} />
           </Container>
-          <Container
-            size='xs'
-            px={0}
-            style={{ margin: 0, width: '100%', textAlign: 'center' }}
-          >
-            <Procedure procedureForm={LODGING_COMPLAINT} />
-            <Arrow />
-            <Procedure procedureForm={SERVE_TO_DEFENDANT} />
-            <Arrow />
-            <Procedure procedureForm={CHECK_DEFENDANT_ANSWER} />
-            <Arrow />
-            <Procedure procedureForm={NOTICE_ARGUMENT_DATE} />
-            <Arrow />
-            <Procedure procedureForm={SENTENCE_JUDGEMENT} />
-            <Arrow />
-            <Procedure procedureForm={EXECUTE_JUDGEMENT} />
-          </Container>
+          <Center>
+            <BranchArrow />
+          </Center>
+          <Space h={50} />
         </div>
+      </MediaQuery>
+      <Container size='sm' px={0}>
+        {mobile ? <MobileProcedureList /> : <ProcedureList />}
       </Container>
     </Container>
   );
