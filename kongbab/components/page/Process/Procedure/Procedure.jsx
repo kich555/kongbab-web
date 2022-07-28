aae2b499f6e46b2106ff49367d09077b4fe09bdf
kich555 <hyunjjang199582@gmail.com>
2022-07-28 16:04:29 +0900

    Remove unused code

--- kongbab/components/page/Process/Procedure/Procedure.jsx
+++ kongbab/components/page/Process/Procedure/Procedure.jsx
@@ -1,6 +1,6 @@
-import { Text, Space, Group, Badge, Center } from '@mantine/core';
+import { Text, Space, Group } from '@mantine/core';
 import useProcedureStyles from '~/Style/page/Process/useProcedureStyles';
-import Sticker from './Sticker/Sticker';
+
 import StickerList from './Sticker/StickerList';
 
 export default function Procedure({ procedureForm }) {
@@ -31,49 +31,6 @@ export default function Procedure({ procedureForm }) {
       <Space h={29} />
       <Group position='center' spacing={24}>
         <StickerList procedureList={procedureList} />
-        {/* <Sticker>
-          <Text weight={700} className={font18}>
-            소송 준비
-          </Text>
-          <Space h={7} />
-          <Text size='sm' color={colors.blue[0]}>
-            질문지 작성, 계약서 등 증거자료 송부
-          </Text>
-        </Sticker>
-        <Sticker icon>
-          <Text weight={500} className={font18}>
-            가처분 신청서 작성
-          </Text>
-        </Sticker>
-        <Sticker>
-          <Text weight={500} className={font18}>
-            가처분 신청서 최종확인 (PDF)
-          </Text>
-        </Sticker>
-        <Sticker icon>
-          <Text weight={500} className={font18}>
-            법원에 접수
-          </Text>
-        </Sticker>
-        <Sticker icon>
-          <Text weight={500} className={font18}>
-            소가 계산, 결제방법 전달
-          </Text>
-        </Sticker>
-        <Sticker>
-          <Text weight={700} className={font18}>
-            인지송달료 결제
-          </Text>
-          <Space h={7} />
-          <Text size='sm' color={colors.blue[0]}>
-            법원에 직접 납부
-          </Text>
-        </Sticker>
-        <Sticker icon>
-          <Text weight={500} className={font18}>
-            가처분 신청완료
-          </Text>
-        </Sticker> */}
       </Group>
     </>
   );
