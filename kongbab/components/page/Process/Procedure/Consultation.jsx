ee55f7d7e9a75ecbbbec0d41c1ba40fdf10be20e
kich555 <hyunjjang199582@gmail.com>
2022-07-28 16:04:49 +0900

    Apply Mobile Responsive on Consultation component

--- kongbab/components/page/Process/Procedure/Consultation.jsx
+++ kongbab/components/page/Process/Procedure/Consultation.jsx
@@ -1,4 +1,5 @@
 import { Container, Text, Center, Space, Group } from '@mantine/core';
+import useResponsive from '~/hooks/useResponsive';
 import useProcedureStyles from '~/Style/page/Process/useProcedureStyles';
 import StickerList from './Sticker/StickerList';
 
@@ -6,7 +7,7 @@ export default function Consultation({ procedureForm }) {
   const { classes, cx } = useProcedureStyles();
   const { centerColumn, lineHeight150, pointer } = classes;
   const { title, desc, link, procedureList } = procedureForm;
-
+  const mobile = useResponsive();
   const handleClick = () => {
     console.log('window', window.Kakao);
     window.Kakao.Channel.addChannel({
@@ -14,30 +15,28 @@ export default function Consultation({ procedureForm }) {
     });
   };
   return (
-    <Container size='xs' px={0}>
-      <Center className={centerColumn}>
-        <Text size='lg' weight={700}>
-          {title}
-        </Text>
-        <Space h={24} />
-        <Text size='sm' className={lineHeight150}>
-          {desc}
-        </Text>
-        <Text
-          onClick={handleClick}
-          component='button'
-          href='#'
-          size='sm'
-          className={cx(lineHeight150, pointer)}
-        >
-          {link}
-        </Text>
-        <Space h={50} />
-        <Group position='center' spacing={24}>
-          <StickerList procedureList={procedureList} />
-        </Group>
-        <Space h={30} />
-      </Center>
-    </Container>
+    <Center className={centerColumn}>
+      <Text size='lg' weight={700}>
+        {title}
+      </Text>
+      <Space h={mobile ? 15 : 24} />
+      <Text size='sm' className={lineHeight150}>
+        {desc}
+      </Text>
+      <Text
+        onClick={handleClick}
+        component='button'
+        href='#'
+        size='sm'
+        className={cx(lineHeight150, pointer)}
+      >
+        {link}
+      </Text>
+      <Space h={mobile ? 40 : 50} />
+      <Group position='center' spacing={24}>
+        <StickerList procedureList={procedureList} />
+      </Group>
+      <Space h={mobile ? 20 : 30} />
+    </Center>
   );
 }
