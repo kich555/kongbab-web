1e38032bf9127c9088f32b702d767213f13bf147
kich555 <hyunjjang199582@gmail.com>
2022-07-29 14:58:24 +0900

    Apply Responsive on Layout

--- kongbab/components/Layout.jsx
+++ kongbab/components/Layout.jsx
@@ -1,17 +1,16 @@
+import { Space } from '@mantine/core';
+import useResponsive from '~/hooks/useResponsive';
 import Footer from './Footer';
 import Nav from './Header/Nav';
 
 export default function Layout({ children }) {
+  const { tablet } = useResponsive();
+
   return (
     <>
       <Nav />
-      <section
-        style={{
-          marginTop: '100px',
-        }}
-      >
-        {children}
-      </section>
+      <Space h={tablet ? 80 : 100} />
+      {children}
       <Footer />
     </>
   );
