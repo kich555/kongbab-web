705a70d48d8f7f01460d0422175ec9e54d495325
kich555 <hyunjjang199582@gmail.com>
2022-07-29 12:35:59 +0900

    Apply Mobile responsive on PriceInfo component

--- kongbab/components/page/Pricing/PriceInfo.jsx
+++ kongbab/components/page/Pricing/PriceInfo.jsx
@@ -1,26 +1,32 @@
-import { Text, Divider } from '@mantine/core';
+import { Text, Divider, Space, MediaQuery } from '@mantine/core';
+
 import CheckIcon from '~/asset/icon/CheckIcon';
+import useResponsive from '~/hooks/useResponsive';
 import usePriceInfo from '~/Style/page/Pricing/usePriceInfoStyles';
 
 export default function PriceInfo({ title, price, options }) {
-  const { classes, theme } = usePriceInfo();
-  const { box, list, listItem, marginTop20 } = classes;
+  const { classes, theme, cx } = usePriceInfo();
+  const { header, box, deskTopBox, list, listItem } = classes;
   const { colors } = theme;
+  const { mobile, tablet } = useResponsive();
 
   const optionList = options.map((option, idx) => {
     if (option === '') {
+      if (mobile) return null;
       return (
         <li key={idx} className={listItem}>
           {' '}
         </li>
       );
     }
+
     if (option.includes('법원출석')) {
       const firstWord = option.split(' ')[0];
       const secondWord = option.split(' ')[1];
       return (
         <li key={idx} className={listItem}>
-          <CheckIcon style={{ marginRight: '15px' }} />
+          <CheckIcon />
+          <Space w={15} />
           <Text weight={700} component='span'>
             {firstWord}
           </Text>{' '}
@@ -30,38 +36,50 @@ export default function PriceInfo({ title, price, options }) {
     }
     return (
       <li key={idx} className={listItem}>
-        <CheckIcon style={{ marginRight: '15px' }} />
+        <CheckIcon />
+        <Space w={15} />
         {option}
       </li>
     );
   });
 
+  const mobileBox = {
+    maxWidth: '325px',
+    padding: '45px 40px 25px 40px',
+    borderRadius: '20px',
+    boxShadow: '0 4px 10px rgba(54, 75, 96, 0.05)',
+  };
+
   return (
-    <li className={box}>
-      <Text size='lg' weight={700}>
-        {title}
-      </Text>
-      <div className={marginTop20}>
-        <Text size='lg' weight={700} component='span'>
+    <MediaQuery smallerThan='md' styles={mobileBox}>
+      <li className={cx(box, deskTopBox)}>
+        <div className={header} />
+        {tablet && <Space h={30} />}
+        <Text size='lg' weight={700}>
+          {title}
+        </Text>
+        <Space h={20} />
+        <Text size='lg' weight={700}>
           {price}
+          <Text size='sm' component='span'>
+            {' '}
+            만원
+          </Text>
         </Text>
-        <Text size='sm' component='span'>
-          {' '}
-          만원
+        <Space h={20} />
+        <Text size='xs' color={colors['blue'][3]}>
+          카드 결제 가능, 부가세 별도
         </Text>
-      </div>
-      <Text size='xs' color={colors['blue'][3]} className={marginTop20}>
-        카드 결제 가능, 부가세 별도
-      </Text>
-      <Divider
-        my='sm'
-        style={{
-          marginTop: '20px',
-          marginBottom: '20px',
-          borderTopColor: '#EDF0F4',
-        }}
-      />
-      <ul className={list}>{optionList}</ul>
-    </li>
+        <Space h={8} />
+        <Divider
+          my='sm'
+          style={{
+            borderTopColor: '#EDF0F4',
+          }}
+        />
+        <Space h={8} />
+        <ul className={list}>{optionList}</ul>
+      </li>
+    </MediaQuery>
   );
 }
