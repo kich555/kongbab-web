211f49b1f3772d10553b5c624ef513664e9d6caf
kich555 <hyunjjang199582@gmail.com>
2022-08-01 19:20:51 +0900

    Refactoring on MainThirdSection component

--- kongbab/app/Style/page/Main/useThirdSectionStyles.jsx
+++ kongbab/app/Style/page/Main/useThirdSectionStyles.jsx
@@ -4,20 +4,6 @@ const useThirdSectionStyles = createStyles((theme) => ({
   wrapper: {
     backgroundColor: theme.colors.gray[0],
   },
-  inner: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: theme.colors.dark[1],
-  },
-  title: {
-    fontSize: '18px',
-  },
-  desc: {
-    fontSize: '40px',
-    lineHeight: '150%',
-  },
 }));
 
 export default useThirdSectionStyles;
--- kongbab/app/components/page/Main/MainThirdSection.jsx
+++ kongbab/app/components/page/Main/MainThirdSection.jsx
@@ -1,34 +1,22 @@
-import { Space, Text } from '@mantine/core';
+import { Space } from '@mantine/core';
 import FeatureList from '~/components/common/FeatureList';
+import MiddleBanner from '~/components/common/MiddleBanner';
 import PriceInfoListSection from '~/components/common/PriceInfo/PriceInfoListSection';
 import useThirdSectionStyles from '~/Style/page/Main/useThirdSectionStyles';
 
 export default function MainThirdSection() {
-  const { classes, theme } = useThirdSectionStyles();
-  const { wrapper, inner, title, desc } = classes;
-  const { colors, primaryColor, white } = theme;
+  const { classes } = useThirdSectionStyles();
+  const { wrapper } = classes;
+
   return (
     <section className={wrapper}>
       <Space h={150} />
       <FeatureList />
-      <div className={inner}>
-        <Space h={100} />
-        <Text
-          color={colors[primaryColor]}
-          align='center'
-          weight={700}
-          className={title}
-        >
-          온라인 명도소송
-        </Text>
-        <Space h={24} />
-        <Text color={white} align='center' weight={700} className={desc}>
-          소송도 얼마든지
-          <br />
-          간편하고 합리적인 요금으로 진행할 수 있습니다
-        </Text>
-        <Space h={100} />
-      </div>
+      <MiddleBanner title='온라인 명도소송'>
+        소송도 얼마든지
+        <br />
+        간편하고 합리적인 요금으로 진행할 수 있습니다
+      </MiddleBanner>
       <PriceInfoListSection />
       <Space h={150} />
     </section>
