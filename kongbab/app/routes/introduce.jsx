182bdb493be074aa8e1eb01e87618e8a1d897a02
kich555 <hyunjjang199582@gmail.com>
2022-08-01 20:22:31 +0900

    Creating Introduce page

--- kongbab/app/routes/introduce.jsx
+++ kongbab/app/routes/introduce.jsx
@@ -1,9 +1,11 @@
 import IntroduceSection from '~/components/page/Introduce/IntroduceSection';
 import IntroduceBanner from '~/components/page/Introduce/IntroduceBanner';
 import INTRODUCE_CONSTANTS from '~/constants/page/introducePageData';
+import MiddleBanner from '~/components/common/MiddleBanner';
+import ProblemSection from '../components/page/Introduce/Problem/ProblemSection';
 
 export default function introduce() {
-  const { INTRODUCE_SECTION } = INTRODUCE_CONSTANTS;
+  const { INTRODUCE_SECTION, PROBLEMS } = INTRODUCE_CONSTANTS;
 
   return (
     <>
@@ -18,6 +20,12 @@ export default function introduce() {
           backgroundColor={section.backgroundColor}
         />
       ))}
+      <ProblemSection problems={PROBLEMS} />
+      <MiddleBanner title='온라인 명도소송'>
+        콩밥은 명도소송 자동화를 통화여
+        <br />
+        법률서비스의 문턱을 획기적으로 낮추는 서비스입니다
+      </MiddleBanner>
     </>
   );
 }
