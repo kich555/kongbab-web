64c56d624d7f58fd6811c4d4d9678839c9e89346
kich555 <hyunjjang199582@gmail.com>
2022-08-01 19:18:13 +0900

    Change Design on EventSection on Pricing page

--- kongbab/app/components/page/Pricing/EventSection.jsx
+++ kongbab/app/components/page/Pricing/EventSection.jsx
@@ -1,40 +1,40 @@
-import { Container, Text, Center, Space } from '@mantine/core';
+import { Container, Text, Center, Space, Image } from '@mantine/core';
 import useEventSectionStyles from '~/Style/page/Pricing/useEventSectionStyles';
 import DefaultButton from '~/components/common/DefaultButton';
 import useResponsive from '~/hooks/useResponsive';
-
+import bannerSrc from '~/asset/image/Pricing_middle_banner_Image.png';
 export default function EventSection(params) {
   const { classes, theme } = useEventSectionStyles();
-  const { colors } = theme;
-  const { horizontal, warpper, font30, buttonWrapper, sticker } = classes;
+  const { colors, primaryColor } = theme;
+  const { horizontal, warpper, font30, buttonWrapper, imageBox } = classes;
   const { tablet } = useResponsive();
   return (
     <>
       <section className={horizontal}>
         <Space h={tablet ? 60 : 150} />
         <Container className={warpper}>
-          <Space h={tablet ? 10 : 8} />
-          <Text color={colors.gray[3]} weight={700}>
-            콩밥이 당신의 부동산을 지켜드립니다
-          </Text>
-          <Space h={tablet ? 15 : 19} />
-          <Text className={font30} weight={700}>
-            2번째부터 10% 할인된 요금으로 만나세요.
-          </Text>
-          <Space h={tablet ? 30 : 38} />
-          <div className={buttonWrapper}>
-            <DefaultButton
-              label='10% 할인 받기'
-              sx={(theme) => ({
-                backgroundColor: theme.colors.gray[0],
-              })}
-            />
-            {tablet ? <Space h={15} /> : <Space w={20} />}
-            <Text size='sm' color={colors.gray[3]} className={sticker}>
-              베이직 요금 제외, 부동산 명의자 기준
+          <div>
+            <Space h={tablet ? 10 : 8} />
+            <Text color={colors.gray[3]} weight={700}>
+              콩밥이 당신의 부동산을 지켜드립니다
             </Text>
+            <Space h={tablet ? 15 : 19} />
+            <Text className={font30} weight={700}>
+              첫 상담, 10% 할인 쿠폰을 드립니다
+            </Text>
+            <Space h={tablet ? 30 : 38} />
+            <div className={buttonWrapper}>
+              <DefaultButton
+                label='10% 할인 받기'
+                sx={(theme) => ({
+                  backgroundColor: colors[primaryColor],
+                })}
+              />
+            </div>
           </div>
+          <Image src={bannerSrc} className={imageBox} />
         </Container>
+        {tablet ? <Space h={15} /> : <Space w={20} />}
       </section>
     </>
   );
