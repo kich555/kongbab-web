ba3bfc7b496ecd4e1ff5495ddae4aaf9f71c8a5e
kich555 <hyunjjang199582@gmail.com>
2022-08-01 13:48:17 +0900

    Refactoring DefaultButton

--- kongbab/app/components/common/DefaultButton.jsx
+++ kongbab/app/components/common/DefaultButton.jsx
@@ -2,14 +2,14 @@ import { Button, Text } from '@mantine/core';
 import useDefaultButtonStyles from '~/Style/components/useDefaultButtonStyles';
 
 export default function DefaultButton({ className = '', label, sx }) {
-  const { classes } = useDefaultButtonStyles();
+  const { classes, cx } = useDefaultButtonStyles();
   const { defaultBtn } = classes;
   return (
     <Button
       sx={sx}
       variant='filled'
       radius={8}
-      className={`${defaultBtn} ${className}`}
+      className={cx(defaultBtn, className)}
     >
       <Text weight={700}>{label}</Text>
     </Button>
