27b26af87dd6ab53388166e3390168713d3c0988
kich555 <hyunjjang199582@gmail.com>
2022-08-01 21:05:25 +0900

    Debugging svg Icon error

--- kongbab/app/asset/icon/ProblemArrow.jsx
+++ kongbab/app/asset/icon/ProblemArrow.jsx
@@ -9,8 +9,8 @@ export default function ProblemArrow({ ...options }) {
       xmlns='http://www.w3.org/2000/svg'
     >
       <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
+        fillRule='evenodd'
+        clipRule='evenodd'
         d='M20.1776 37.5293L0 0.188597L20.0873 8.21983L40 0.355493L20.1776 37.5293Z'
         fill='#C4C4C4'
       />
