f85dab61168bd2147e043a7fc39ae210a9daabf3
kich555 <hyunjjang199582@gmail.com>
2022-08-01 21:07:27 +0900

    Refactoring MainThirdSection component

--- kongbab/app/components/page/Main/MainThirdSection.jsx
+++ kongbab/app/components/page/Main/MainThirdSection.jsx
@@ -4,7 +4,7 @@ import FeatureList from '~/components/common/FeatureList';
 import MiddleBanner from '~/components/common/MiddleBanner';
 import PriceInfoListSection from '~/components/common/PriceInfo/PriceInfoListSection';
 import useThirdSectionStyles from '~/Style/page/Main/useThirdSectionStyles';
-import MAIN_CONSTANTS from '../../../constants/page/mainPageData';
+import MAIN_CONSTANTS from '~/constants/page/mainPageData';
 
 export default function MainThirdSection() {
   const { classes } = useThirdSectionStyles();
@@ -17,11 +17,7 @@ export default function MainThirdSection() {
       <MiddleBanner
         title={MIDDLE_BANNER.title}
         desc={parse(MIDDLE_BANNER.desc)}
-      >
-        소송도 얼마든지
-        <br />
-        간편하고 합리적인 요금으로 진행할 수 있습니다
-      </MiddleBanner>
+      />
       <PriceInfoListSection />
       <Space h={150} />
     </section>
