92b666f2606fc1f3f39814dfaaddd2e03a97fa35
kich555 <hyunjjang199582@gmail.com>
2022-08-01 14:11:32 +0900

    Creating main page

--- kongbab/app/routes/index.jsx
+++ kongbab/app/routes/index.jsx
@@ -1,9 +1,11 @@
+import MainFourthSection from '~/components/page/Main/MainFourthSection';
 import MainThirdSection from '~/components/page/Main/MainThirdSection';
 
 export default function Index() {
   return (
     <>
       <MainThirdSection />
+      <MainFourthSection />
     </>
   );
 }
