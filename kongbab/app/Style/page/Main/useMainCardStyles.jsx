df98fbda1c56cc5dbeb0ba18e18a1680d24d1bd5
kich555 <hyunjjang199582@gmail.com>
2022-08-02 20:14:27 +0900

    Create MainCards styles

---
+++ kongbab/app/Style/page/Main/useMainCardStyles.jsx
@@ -0,0 +1,50 @@
+import { createStyles } from '@mantine/core';
+
+export const useMainCardStyles = createStyles((theme) => ({
+  wrapper: {
+    flexGrow: '1',
+    borderRadius: '20px',
+    backgroundColor: theme.colors.gray[0],
+  },
+  font20: {
+    fontSize: '20px',
+  },
+  lineHeight150: {
+    lineHeight: '150%',
+  },
+  image: {
+    width: '320px',
+    height: 'auto',
+  },
+}));
+
+export const useMainLargeCardStyles = createStyles((theme) => ({
+  wrapper: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    borderRadius: '20px',
+    backgroundColor: theme.colors.gray[0],
+    padding: '48px 45px 57px 70px',
+  },
+  inner: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  contentWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  font20: {
+    fontSize: '20px',
+  },
+  lineHeight150: {
+    lineHeight: '150%',
+  },
+  image: {
+    width: '453px',
+    height: 'auto',
+  },
+}));
