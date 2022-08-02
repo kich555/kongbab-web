c4b53a5147eb5542f406d182f5f8afbddb55b861
kich555 <hyunjjang199582@gmail.com>
2022-08-02 20:14:40 +0900

    Create MainCardSection styles

---
+++ kongbab/app/Style/page/Main/useMainCardSectionStyles.jsx
@@ -0,0 +1,8 @@
+import { createStyles } from '@mantine/core';
+
+const useMainCardSectionStyles = createStyles((theme) => ({
+  font40: { fontSize: '40px' },
+  font20: { fontSize: '20px' },
+}));
+
+export default useMainCardSectionStyles;
