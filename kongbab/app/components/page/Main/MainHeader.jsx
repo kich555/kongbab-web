fb06691ec3f1f86db1bc1af2f03345695153f349
kich555 <hyunjjang199582@gmail.com>
2022-08-04 12:21:24 +0900

    Create MainHeader component

--- kongbab/app/components/page/Main/MainHeader.jsx
+++ kongbab/app/components/page/Main/MainHeader.jsx
@@ -1,8 +1,10 @@
-import { Carousel } from '@mantine/carousel';
+import { useRef } from 'react';
 import { Container, Group, Image, Space, Text } from '@mantine/core';
 import DefaultButton from '~/components/common/DefaultButton';
 import useMainHeaderStyles from '~/Style/page/Main/useMainHeaderStyles';
-
+import Autoplay from 'embla-carousel-autoplay';
+import { Carousel } from '@mantine/carousel';
+import Main_Banner from '~/asset/image/Carousel/Main_Banner.png';
 import Carousel_A_1 from '~/asset/image/Carousel/Carousel_A_1.png';
 import Carousel_A_2 from '~/asset/image/Carousel/Carousel_A_2.png';
 import Carousel_A_3 from '~/asset/image/Carousel/Carousel_A_3.png';
@@ -12,30 +14,120 @@ import Carousel_B_3 from '~/asset/image/Carousel/Carousel_B_3.png';
 
 export default function MainHeader(params) {
   const { classes, theme, cx } = useMainHeaderStyles();
-  const { backgorund, wrapper, shadow, button, font18, font40, lineHeight150 } =
-    classes;
-  const { colors, primaryColor } = theme;
-  const CarouselA = [Carousel_A_1, Carousel_A_2, Carousel_A_3];
-  const CarouselB = [Carousel_B_1, Carousel_B_2, Carousel_B_3];
 
-  const items = CarouselA.map((item, idx) => (
-    <Carousel.Slide key={`carousel-A-Slide-${idx}`}>
-      <>
-        <Space h={30} />
-        <Image
-          src={item}
-          alt={`carousel-A-image-${idx}`}
-          width={242}
-          className={shadow}
-        />
-        <Space h={30} />
-      </>
-    </Carousel.Slide>
-  ));
+  const {
+    backgorund,
+    wrapper,
+    inner,
+    shadow,
+    button,
+    font18,
+    font40,
+    lineHeight150,
+    flewRow,
+  } = classes;
+  const { colors, primaryColor } = theme;
+  // const CarouselA = [
+  //   Carousel_A_1,
+  //   Carousel_A_2,
+  //   Carousel_A_3,
+  //   Carousel_A_1,
+  //   Carousel_A_2,
+  //   Carousel_A_3,
+  //   Carousel_A_1,
+  //   Carousel_A_2,
+  //   Carousel_A_3,
+  //   Carousel_A_1,
+  //   Carousel_A_2,
+  //   Carousel_A_3,
+  //   Carousel_A_1,
+  //   Carousel_A_2,
+  //   Carousel_A_3,
+  //   Carousel_A_1,
+  //   Carousel_A_2,
+  //   Carousel_A_3,
+  //   Carousel_A_1,
+  //   Carousel_A_2,
+  //   Carousel_A_3,
+  //   Carousel_A_1,
+  //   Carousel_A_2,
+  //   Carousel_A_3,
+  //   Carousel_A_1,
+  //   Carousel_A_2,
+  //   Carousel_A_3,
+  // ];
+  // const CarouselB = [
+  //   Carousel_B_1,
+  //   Carousel_B_2,
+  //   Carousel_B_3,
+  //   Carousel_B_1,
+  //   Carousel_B_2,
+  //   Carousel_B_3,
+  //   Carousel_B_1,
+  //   Carousel_B_2,
+  //   Carousel_B_3,
+  //   Carousel_B_1,
+  //   Carousel_B_2,
+  //   Carousel_B_3,
+  //   Carousel_B_1,
+  //   Carousel_B_2,
+  //   Carousel_B_3,
+  //   Carousel_B_1,
+  //   Carousel_B_2,
+  //   Carousel_B_3,
+  //   Carousel_B_1,
+  //   Carousel_B_2,
+  //   Carousel_B_3,
+  //   Carousel_B_1,
+  //   Carousel_B_2,
+  //   Carousel_B_3,
+  //   Carousel_B_1,
+  //   Carousel_B_2,
+  //   Carousel_B_3,
+  //   Carousel_B_1,
+  //   Carousel_B_2,
+  //   Carousel_B_3,
+  // ];
+  // const autoplayA = useRef(
+  //   Autoplay({ delay: 3000 }, (emblaRoot) => emblaRoot.parentElement)
+  // );
+  // const autoplayB = useRef(
+  //   Autoplay({ delay: 3000 }, (emblaRoot) => emblaRoot.parentElement)
+  // );
+
+  // const items = CarouselA.map((item, idx) => (
+  //   <Carousel.Slide key={`carousel-A-Slide-${idx}`}>
+  //     <>
+  //       <Image
+  //         src={item}
+  //         alt={`carousel-A-image-${idx}`}
+  //         width={242}
+  //         className={shadow}
+  //         withPlaceholder
+  //       />
+  //     </>
+  //   </Carousel.Slide>
+  // ));
+
+  // const itemsB = CarouselB.map((item, idx) => (
+  //   <Carousel.Slide key={`carousel-A-Slide-${idx}`}>
+  //     <>
+  //       <Image
+  //         src={item}
+  //         alt={`carousel-A-image-${idx}`}
+  //         width={242}
+  //         className={shadow}
+  //         withPlaceholder
+  //       />
+  //     </>
+  //   </Carousel.Slide>
+  // ));
+
   return (
     <section className={backgorund}>
-      <Container px={0}>
-        <div className={wrapper}>
+      <Space w={180} />
+      <Container px={0} className={wrapper}>
+        <div className={inner}>
           <div>
             <Text color={colors[primaryColor]} weight={500} className={font18}>
               온라인 명도소송
@@ -59,14 +151,50 @@ export default function MainHeader(params) {
               label='카톡 상담신청'
             />
           </div>
-          <Carousel
-            orientation='vertical'
-            height={540}
-            sx={{ maxWidth: 530 }}
-            mx='auto'
-          >
-            {items}
-          </Carousel>
+          <Image src={Main_Banner} width={560} />
+          {/* <Group spacing={10}>
+            <Carousel
+              orientation='vertical'
+              loop
+              withControls={false}
+              initialSlide={1}
+              slideSize='33.33%'
+              height={540}
+              sx={{ maxWidth: 530 }}
+              draggable={false}
+              plugins={[autoplayA.current]}
+              onMouseEnter={autoplayA.current.stop}
+              onMouseLeave={autoplayA.current.reset}
+              styles={{
+                container: {
+                  transition: 'all 3s linear !important',
+                },
+              }}
+            >
+              {items}
+            </Carousel>
+            <Carousel
+              orientation='vertical'
+              loop
+              align={0.7}
+              withControls={false}
+              initialSlide={1}
+              slideSize='33.33%'
+              height={540}
+              sx={{ maxWidth: 530 }}
+              draggable={false}
+              plugins={[autoplayB.current]}
+              onMouseEnter={autoplayB.current.stop}
+              onMouseLeave={autoplayB.current.reset}
+              styles={{
+                container: {
+                  transition: 'all 3s linear !important',
+                },
+              }}
+            >
+              {itemsB}
+            </Carousel>
+          </Group> */}
         </div>
         <Space w={180} />
       </Container>
