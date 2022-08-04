4fbbba4d794671d5f19303f0b5e7d0563557535d
kich555 <hyunjjang199582@gmail.com>
2022-08-04 12:12:53 +0900

    Redesgin process page

--- kongbab/app/routes/md/process.jsx
+++ kongbab/app/routes/md/process.jsx
@@ -1,4 +1,4 @@
-import { Container } from '@mantine/core';
+import { Container, Space } from '@mantine/core';
 import ProcessConsultationSection from '~/components/page/Process/ProcessConsultationSection';
 import LitigationProcedureTitle from '~/components/page/Process/Procedure/LitigationProcedureTitle';
 import LitigationProcedure from '~/components/page/Process/Procedure/LitigationProcedure';
@@ -10,11 +10,15 @@ export default function process() {
     <section>
       <Container style={{ padding: '0 26px' }}>
         <LitigationInfo />
-        <KakaoSection />
         <LitigationProcedureTitle />
-        <LitigationProcedure />
-        <ProcessConsultationSection />
+        <Space h={40} />
       </Container>
+      <section style={{ backgroundColor: '#F7F7F7' }}>
+        <LitigationProcedure />
+        <Space h={100} />
+      </section>
+      <KakaoSection />
+      <ProcessConsultationSection />
     </section>
   );
 }
