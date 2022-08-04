96f48d19343f2a9abac6a4d249c7dd4db4a4a2ec
kich555 <hyunjjang199582@gmail.com>
2022-08-04 12:13:13 +0900

    Add MainHeader component on main page

--- kongbab/app/routes/md/main.jsx
+++ kongbab/app/routes/md/main.jsx
@@ -1,10 +1,12 @@
 import MainThirdSection from '~/components/page/Main/MainThirdSection';
 import CommonFooterSection from '~/components/common/CommonFooterSection';
 import MainSecondSection from '~/components/page/Main/MainSecondSection';
+import MainHeader from '~/components/page/Main/MainHeader';
 
 export default function Main() {
   return (
     <>
+      <MainHeader />
       <MainSecondSection />
       <MainThirdSection />
       <CommonFooterSection />
