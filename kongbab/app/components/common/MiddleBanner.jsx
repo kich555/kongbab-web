0b2e65476a038ef23d4884f450f95a7f91fdb04c
kich555 <hyunjjang199582@gmail.com>
2022-08-05 16:13:00 +0900

    Add Intersection Observer & Animation on MiddleBanner component

--- kongbab/app/components/common/MiddleBanner.jsx
+++ kongbab/app/components/common/MiddleBanner.jsx
@@ -1,27 +1,40 @@
+import { useRef } from 'react';
 import { Space, Text } from '@mantine/core';
-
+import { useIntersection } from '@mantine/hooks';
 import useMiddleBannerStyles from '~/constants/common/useMiddleBannerStyles';
 
 export default function MiddleBanner({ title, desc }) {
+  const containerRef = useRef();
+  const { ref, entry } = useIntersection({
+    root: containerRef.current,
+  });
   const { classes, theme } = useMiddleBannerStyles();
   const { wrapper, font18, font40 } = classes;
   const { colors, primaryColor, white } = theme;
+
+  console.log('entry?.isIntersecting', entry?.isIntersecting);
   return (
-    <div className={wrapper}>
-      <Space h={100} />
-      <Text
-        color={colors[primaryColor]}
-        align='center'
-        weight={700}
-        className={font18}
+    <div>
+      <div
+        className={wrapper}
+        ref={ref}
+        style={{ backgroundPosition: entry?.isIntersecting && 'left' }}
       >
-        {title}
-      </Text>
-      <Space h={24} />
-      <Text color={white} align='center' weight={700} className={font40}>
-        {desc}
-      </Text>
-      <Space h={100} />
+        <Space h={100} />
+        <Text
+          color={colors[primaryColor]}
+          align='center'
+          weight={700}
+          className={font18}
+        >
+          {title}
+        </Text>
+        <Space h={24} />
+        <Text color={white} align='center' weight={700} className={font40}>
+          {desc}
+        </Text>
+        <Space h={100} />
+      </div>
     </div>
   );
 }
--- kongbab/app/constants/common/useMiddleBannerStyles.jsx
+++ kongbab/app/constants/common/useMiddleBannerStyles.jsx
@@ -6,7 +6,13 @@ const useMiddleBannerStyles = createStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: theme.colors.dark[1],
+    // backgroundColor: theme.colors.dark[1],
+    background: 'linear-gradient(to left, white 50%, black 50%) right',
+    backgroundSize: '200% 100%',
+    transition: 'all 10s ease',
+    // '&:hover': {
+    //   backgroundPosition: 'left',
+    // },
   },
   font18: {
     fontSize: '18px',
