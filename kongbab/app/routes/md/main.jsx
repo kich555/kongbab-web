0a2fb5893920790c30082321f4ea25f02db41a93
kich555 <hyunjjang199582@gmail.com>
2022-08-06 00:41:34 +0900

    Creating main page

--- kongbab/app/routes/md/main.jsx
+++ kongbab/app/routes/md/main.jsx
@@ -2,6 +2,7 @@ import MainThirdSection from '~/components/page/Main/MainThirdSection';
 import CommonFooterSection from '~/components/common/CommonFooterSection';
 import MainSecondSection from '~/components/page/Main/MainSecondSection';
 import MainHeader from '~/components/page/Main/MainHeader';
+import ConsultationForm from '~/components/page/Main/ConsultationForm';
 
 export default function Main() {
   return (
@@ -9,6 +10,7 @@ export default function Main() {
       <MainHeader />
       <MainSecondSection />
       <MainThirdSection />
+      <ConsultationForm />
       <CommonFooterSection />
     </>
   );
