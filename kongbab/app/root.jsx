1cc793f0373d726e6ed2619db4282dfc7ae42bb2
kich555 <hyunjjang199582@gmail.com>
2022-08-07 02:59:51 +0900

    Apply Mantine Notification on root

--- kongbab/app/root.jsx
+++ kongbab/app/root.jsx
@@ -5,6 +5,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from '@remix-run/react';
 import { MantineProvider } from '@mantine/core';
 import { StylesPlaceholder } from '@mantine/remix';
@@ -12,9 +13,17 @@ import BASIC_THEME from '~/constants/theme';
 import { json } from '@remix-run/node';
 import styles from '~/Style/reset.css';
 import Layout from '~/components/common/Layout';
+import { NotificationsProvider } from '@mantine/notifications';
+// import { getEnv } from './env.server';
 
 export async function loader() {
-  return json({ KAKAO_KEY: process.env.REACT_APP_KEY });
+  return json({
+    ENV: {
+      EMAIL_JS_ID: process.env.EMAIL_JS_ID,
+      EMAIL_JS_TEMPLETE_ID: process.env.EMAIL_JS_TEMPLETE_ID,
+      EMAIL_JS_PUBLIC_KEY: process.env.EMAIL_JS_PUBLIC_KEY,
+    },
+  });
 }
 
 export const meta = () => ({
@@ -33,36 +42,45 @@ export function links() {
 }
 
 export default function App() {
+  const data = useLoaderData();
+
   return (
     <MantineProvider theme={BASIC_THEME} withGlobalStyles withNormalizeCSS>
-      <html lang='ko'>
-        <head>
-          <Meta />
-          <Links />
-          <StylesPlaceholder />
-        </head>
-        <body>
-          <>
-            <script src='https://developers.kakao.com/sdk/js/kakao.js'></script>
-            <script
-              async
-              id='kakao-init'
-              dangerouslySetInnerHTML={{
-                __html: `
+      <NotificationsProvider>
+        <html lang='ko'>
+          <head>
+            <Meta />
+            <Links />
+            <StylesPlaceholder />
+          </head>
+          <body>
+            <>
+              <script src='https://developers.kakao.com/sdk/js/kakao.js'></script>
+              <script
+                async
+                id='kakao-init'
+                dangerouslySetInnerHTML={{
+                  __html: `
               Kakao.init('JAVASCRIPT_KEY');
               console.log(Kakao.isInitialized());
               `,
-              }}
-            />
-          </>
-          <Layout>
-            <Outlet />
-          </Layout>
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </body>
-      </html>
+                }}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+                }}
+              />
+            </>
+            <Layout>
+              <Outlet />
+            </Layout>
+            <ScrollRestoration />
+            <Scripts />
+            <LiveReload />
+          </body>
+        </html>
+      </NotificationsProvider>
     </MantineProvider>
   );
 }
