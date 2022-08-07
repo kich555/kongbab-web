4415b5127a550ee735169fbbc51cc3b5c7b5ca01
kich555 <hyunjjang199582@gmail.com>
2022-08-07 15:56:25 +0900

    Add Padding for Mobile on ProcessConsultationSection

--- kongbab/app/components/page/Process/ProcessConsultationSection.jsx
+++ kongbab/app/components/page/Process/ProcessConsultationSection.jsx
@@ -1,4 +1,4 @@
-import { Space, Text, useMantineTheme } from '@mantine/core';
+import { Container, Space, Text, useMantineTheme } from '@mantine/core';
 import asset from '~/asset/image/Process_Footer_Banner.png';
 import mobileAsset from '~/asset/image/Mobile_Process_Footer_Banner.png';
 import useResponsive from '~/hooks/useResponsive';
@@ -9,22 +9,24 @@ export default function ProcessConsultationSection() {
   const { white } = theme;
   const { mobile } = useResponsive();
   return (
-    <ConsultationCard asset={mobile ? mobileAsset : asset}>
-      <Space h={25} />
-      <Text
-        align='center'
-        color={white}
-        weight={700}
-        style={
-          mobile
-            ? { fontSize: '20px', lineHeight: '150%' }
-            : { fontSize: '30px', lineHeight: '150%' }
-        }
-      >
-        명도소송은 신속해야 합니다
-        <br />
-        빠른 대처로 피해를 최소화시키세요
-      </Text>
-    </ConsultationCard>
+    <Container px={mobile ? 26 : 0}>
+      <ConsultationCard asset={mobile ? mobileAsset : asset}>
+        <Space h={25} />
+        <Text
+          align='center'
+          color={white}
+          weight={700}
+          style={
+            mobile
+              ? { fontSize: '20px', lineHeight: '150%' }
+              : { fontSize: '30px', lineHeight: '150%' }
+          }
+        >
+          명도소송은 신속해야 합니다
+          <br />
+          빠른 대처로 피해를 최소화시키세요
+        </Text>
+      </ConsultationCard>
+    </Container>
   );
 }
