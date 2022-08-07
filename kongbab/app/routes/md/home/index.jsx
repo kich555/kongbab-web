9a6eeda2b96866ab38cb554dd084fd3fbb1d25b3
kich555 <hyunjjang199582@gmail.com>
2022-08-07 11:10:54 +0900

    Apply mobile responsive on home page

--- kongbab/app/routes/md/home/index.jsx
+++ kongbab/app/routes/md/home/index.jsx
@@ -20,6 +20,7 @@ import {
 } from '@mantine/core';
 import useFormBoxStyles from '~/Style/page/Main/useFormBoxStyles';
 import Garbage from '~/asset/icon/Garbage';
+import useResponsive from '~/hooks/useResponsive';
 
 export default function Home() {
   const form = useRef();
@@ -77,146 +78,154 @@ export default function Home() {
       });
     }
   };
-
+  const { mobile } = useResponsive();
   return (
     <>
       <MainHeader />
       <MainSecondSection />
       <MainThirdSection />
-      <ConsultationForm>
-        <Form
-          method='post'
-          ref={form}
-          encType='multipart/form-data'
-          onSubmit={sendEmail}
-        >
-          <label>
-            <Text size='md' weight={500}>
-              이름
-            </Text>
-            <Space h={10} />
-            <div className={inputWrapper}>
-              <input
-                type='text'
-                name='name'
-                placeholder='김임차'
-                className={cx(textInput, error)}
-              />
-            </div>
-            {/* {errors?.slug ? (
-        <em className='text-red-600'>{errors.slug}</em>
-      ) : null} */}
-          </label>
-
-          <Space h={35} />
-
-          <label>
-            <Text size='md' weight={500} component='div'>
-              휴대폰 번호{' '}
-              <Text
-                size='md'
-                color={colors.gray[3]}
-                weight={500}
-                component='span'
-              >
-                (숫자만 입력)
-              </Text>
-            </Text>
-            <Space h={10} />
-            <div className={inputWrapper}>
-              <input
-                type='number'
-                name='phoneNumber'
-                placeholder='01012345678'
-                className={textInput}
-              />
-            </div>
-            {/* {errors?.slug ? (
-        <em className='text-red-600'>{errors.slug}</em>
-      ) : null} */}
-          </label>
-
-          <Space h={35} />
-
-          <label>
-            <Text size='md' weight={500} component='div'>
-              임대차 계약서{' '}
-              <Text
-                size='md'
-                color={colors.gray[3]}
-                weight={500}
-                component='span'
-              >
-                (선택)
-              </Text>
-            </Text>
-            <Space h={10} />
-            <FileButton
-              name='files'
-              onChange={setFiles}
-              accept='image/png,image/jpeg'
-              multiple
+      {!mobile && (
+        <ConsultationForm>
+          <>
+            <Form
+              method='post'
+              ref={form}
+              encType='multipart/form-data'
+              onSubmit={sendEmail}
             >
-              {(props) => (
-                <Button radius='md' {...props} className={button}>
-                  파일 첨부
-                </Button>
-              )}
-            </FileButton>
-            {/* {errors?.slug ? (
+              <label>
+                <Text size='md' weight={500}>
+                  이름
+                </Text>
+                <Space h={10} />
+                <div className={inputWrapper}>
+                  <input
+                    type='text'
+                    name='name'
+                    placeholder='김임차'
+                    className={cx(textInput, error)}
+                  />
+                </div>
+                {/* {errors?.slug ? (
         <em className='text-red-600'>{errors.slug}</em>
       ) : null} */}
-          </label>
+              </label>
 
-          <List size='sm' mt={5}>
-            <Group spacing={10}>
-              {files.map((file, index) => (
-                <List.Item key={index}>
-                  <div className={cx(inputWrapper, fileInput)}>
-                    <input className={textInput} value={file.name} disabled />
-                    <Garbage style={{ marginRight: '16px' }} />
-                  </div>
-                </List.Item>
-              ))}
-            </Group>
-          </List>
+              <Space h={35} />
 
-          <Space h={35} />
-          <Checkbox
-            onChange={(e) => setIsChecked(e.currentTarget.checked)}
-            checked={isChecked}
-            value={isChecked}
-            label={
-              <>
-                (필수){' '}
-                <Anchor
-                  size='md'
-                  href='https://mantine.dev'
-                  target='_blank'
-                  color='#3787E4'
+              <label>
+                <Text size='md' weight={500} component='div'>
+                  휴대폰 번호{' '}
+                  <Text
+                    size='md'
+                    color={colors.gray[3]}
+                    weight={500}
+                    component='span'
+                  >
+                    (숫자만 입력)
+                  </Text>
+                </Text>
+                <Space h={10} />
+                <div className={inputWrapper}>
+                  <input
+                    type='number'
+                    name='phoneNumber'
+                    placeholder='01012345678'
+                    className={textInput}
+                  />
+                </div>
+                {/* {errors?.slug ? (
+        <em className='text-red-600'>{errors.slug}</em>
+      ) : null} */}
+              </label>
+
+              <Space h={35} />
+
+              <label>
+                <Text size='md' weight={500} component='div'>
+                  임대차 계약서{' '}
+                  <Text
+                    size='md'
+                    color={colors.gray[3]}
+                    weight={500}
+                    component='span'
+                  >
+                    (선택)
+                  </Text>
+                </Text>
+                <Space h={10} />
+                <FileButton
+                  name='files'
+                  onChange={setFiles}
+                  accept='image/png,image/jpeg'
+                  multiple
                 >
-                  개인정보 처리방침
-                </Anchor>{' '}
-                동의
-              </>
-            }
-            size='md'
-            color='black'
-            classNames={{ input, label }}
-          />
-          <Space h={42} />
-          <Button
-            type='submit'
-            radius='md'
-            name='intent'
-            disabled={isLoading}
-            loading={isLoading}
-            className={submmit}
-          >
-            카톡 상담신청
-          </Button>
-        </Form>
-      </ConsultationForm>
+                  {(props) => (
+                    <Button radius='md' {...props} className={button}>
+                      파일 첨부
+                    </Button>
+                  )}
+                </FileButton>
+                {/* {errors?.slug ? (
+        <em className='text-red-600'>{errors.slug}</em>
+      ) : null} */}
+              </label>
+
+              <List size='sm' mt={5}>
+                <Group spacing={10}>
+                  {files.map((file, index) => (
+                    <List.Item key={index}>
+                      <div className={cx(inputWrapper, fileInput)}>
+                        <input
+                          className={textInput}
+                          value={file.name}
+                          disabled
+                        />
+                        <Garbage style={{ marginRight: '16px' }} />
+                      </div>
+                    </List.Item>
+                  ))}
+                </Group>
+              </List>
+
+              <Space h={35} />
+              <Checkbox
+                onChange={(e) => setIsChecked(e.currentTarget.checked)}
+                checked={isChecked}
+                value={isChecked}
+                label={
+                  <>
+                    (필수){' '}
+                    <Anchor
+                      size='md'
+                      href='https://mantine.dev'
+                      target='_blank'
+                      color='#3787E4'
+                    >
+                      개인정보 처리방침
+                    </Anchor>{' '}
+                    동의
+                  </>
+                }
+                size='md'
+                color='black'
+                classNames={{ input, label }}
+              />
+              <Space h={42} />
+              <Button
+                type='submit'
+                radius='md'
+                name='intent'
+                disabled={isLoading}
+                loading={isLoading}
+                className={submmit}
+              >
+                카톡 상담신청
+              </Button>
+            </Form>
+          </>
+        </ConsultationForm>
+      )}
       <CommonFooterSection />
     </>
   );
