b6be90764276bd40a400417ff29d8ef66edc4fd0
kich555 <hyunjjang199582@gmail.com>
2022-08-07 15:54:20 +0900

    Add Favicon & Og tags on root

--- kongbab/app/root.jsx
+++ kongbab/app/root.jsx
@@ -26,17 +26,137 @@ export async function loader() {
   });
 }
 
-export const meta = () => ({
-  charset: 'utf-8',
-  title: 'cong-bab',
-  viewport: 'width=device-width,initial-scale=1',
-});
+export const meta = () => (
+  {
+    charset: 'utf-8',
+    title: 'cong-bab',
+    viewport: 'width=device-width,initial-scale=1',
+    'og:type': 'website',
+    'og:url': 'http://congbab.com/md/home',
+    'og:title': '명도소송 콩밥 | 빠르고 간편한 소송을 위한 법률서비스',
+    'og:image': '/og/og-image.png',
+    'og:site_name': '명도소송 콩밥 | 빠르고 간편한 소송을 위한 법률서비스',
+    'og:locale': 'ko_KR',
+    'og:image:width': '1200',
+    'og:image:height': '630',
+    'twitter:card': 'summary_large_image',
+    'twitter:title': '명도소송 콩밥 | 빠르고 간편한 소송을 위한 법률서비스',
+    'twitter:image': '/og/og-image.png',
+  },
+  // {
+  //   name: 'msapplication-TileColor',
+  //   content: '#ffffff',
+  // },
+  // {
+  //   name: 'msapplication-TileImage',
+  //   content: '/ms-icon-144x144.png',
+  // },
+  // {
+  //   name: 'theme-color',
+  //   content: '#ffffff',
+  // },
+  // { property: 'og:type', content: 'website' },
+  // { property: 'og:url', content: 'http://congbab.com/root.jsx' },
+  // {
+  //   property: 'og:title',
+  //   content: '명도소송 콩밥 | 빠르고 간편한 소송을 위한 법률서비스',
+  // },
+  // { property: 'og:image', content: '/og/og-image.png' },
+  // {
+  //   property: 'og:site_name',
+  //   content: '명도소송 콩밥 | 빠르고 간편한 소송을 위한 법률서비스',
+  // },
+  // { property: 'og:locale', content: 'ko_KR' },
+  // { property: 'og:image:width', content: '1200' },
+  // { property: 'og:image:height', content: '630' },
+  // { property: 'og:image:height', content: '630' },
+  // { name: 'twitter:card', content: 'summary_large_image' },
+  // {
+  //   name: 'twitter:title',
+  //   content: '명도소송 콩밥 | 빠르고 간편한 소송을 위한 법률서비스',
+  // },
+  // { name: 'twitter:image', content: '/og/og-image.png' }
+);
 
 export function links() {
   return [
     {
       rel: 'stylesheet',
       href: styles,
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '57x57',
+      href: '/favicon/apple-icon-57x57.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '60x60',
+      href: '/favicon/apple-icon-60x60.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '72x72',
+      href: '/favicon/apple-icon-72x72.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '76x76',
+      href: '/favicon/apple-icon-76x76.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '114x114',
+      href: '/favicon/apple-icon-114x114.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '120x120',
+      href: '/favicon/apple-icon-120x120.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '144x144',
+      href: '/favicon/apple-icon-144x144.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '152x152',
+      href: '/favicon/apple-icon-152x152.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/favicon/apple-icon-180x180.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '192x192',
+      href: '/favicon/android-icon-192x192.png',
+    },
+
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '96x96',
+      href: '/favicon/favicon-96x96.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/favicon/manifest.json',
     },
   ];
 }
