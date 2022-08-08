111fffc202a011c62b832d0c33fa0a0d75a5aba4
kich555 <hyunjjang199582@gmail.com>
2022-08-09 03:14:10 +0900

    AddKakaoChannel function on DefaultButton

--- app/components/common/DefaultButton.jsx
+++ app/components/common/DefaultButton.jsx
@@ -1,7 +1,9 @@
 import { Button, Text } from '@mantine/core';
+import useAddKakaoChannel from '~/hooks/useAddKakaoChannel';
 import useDefaultButtonStyles from '~/Style/components/useDefaultButtonStyles';
 
 export default function DefaultButton({ className = '', label, sx }) {
+  const { addKakaoChannel } = useAddKakaoChannel();
   const { classes, cx } = useDefaultButtonStyles();
   const { defaultBtn } = classes;
   return (
@@ -10,6 +12,7 @@ export default function DefaultButton({ className = '', label, sx }) {
       variant='filled'
       radius={8}
       className={cx(defaultBtn, className)}
+      onClick={addKakaoChannel}
     >
       <Text weight={700}>{label}</Text>
     </Button>
