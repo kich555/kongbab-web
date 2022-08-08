e8986ddba5c4ddaebca48a02fca9b667d5a19a1c
kich555 <hyunjjang199582@gmail.com>
2022-08-09 02:21:34 +0900

    Refactoring addKakaoChannel hook

--- app/hooks/useAddKakaoChannel.jsx
+++ app/hooks/useAddKakaoChannel.jsx
@@ -1,16 +1,17 @@
 import { useCallback } from 'react';
 
-export default function useAddKakaoChannel(params) {
+export default function useAddKakaoChannel() {
   // const channelId = '_eXERxj';
-  const channelId = '_xhhXIxj';
-  const addChannel = useCallback(() => {
+  const channelId = '_eXERxj';
+
+  const addKakaoChannel = useCallback(() => {
     const isBrowser = !!window;
     if (!isBrowser) return;
     const { Kakao } = window;
-    Kakao?.Channel.createChatButton({
+    Kakao?.Channel.addChannel({
       channelPublicId: channelId, // Kakao Channel 홈 URL에 명시된 id로 설정
     });
   }, [channelId]);
 
-  return { addChannel };
+  return { addKakaoChannel };
 }
