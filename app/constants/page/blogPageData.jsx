4a72763b31d16451f077c4d5de0c7d6010dd2a4c
kich555 <hyunjjang199582@gmail.com>
2022-08-15 00:43:17 +0900

    Blog Thumbnail Image test

--- app/constants/page/blogPageData.jsx
+++ app/constants/page/blogPageData.jsx
@@ -3,9 +3,10 @@ const BLOG_CONSTANTS = {
   LIST: [
     {
       thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
+        'https://postfiles.pstatic.net/MjAyMjA4MTBfMzAg/MDAxNjYwMTE2NDQyOTA3.nLcFwQbvP2-RP3t2nywazYyASgPqmy33N6mOXNi5pGEg.Sc9XQLr8vIsCoVV7l1OtIbL2ZgXo1aMcgmg7KuXtHZEg.PNG.congbablaw/%EC%9B%B9%EC%82%AC%EC%9D%B4%ED%8A%B8_%EB%AA%85%EB%8F%84%EC%86%8C%EC%86%A1_%EC%BD%A9%EB%B0%A5.png?type=w773',
+
+      link: 'https://blog.naver.com/PostView.naver?blogId=congbablaw&logNo=222846568137&redirect=Dlog&widgetTypeCall=true&directAccess=false',
+      category: '콩밥소개',
       title: '나홀로 소송, 전체 프로세스 돌아보기',
       desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
     },
@@ -61,7 +62,7 @@ const BLOG_CONSTANTS = {
       thumbnail:
         'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
       link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
+      category: '소송사례',
       title: '나홀로 소송, 전체 프로세스 돌아보기',
       desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
     },
@@ -73,94 +74,94 @@ const BLOG_CONSTANTS = {
       title: '나홀로 소송, 전체 프로세스 돌아보기',
       desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
     },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
-    {
-      thumbnail:
-        'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
-      link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
-      category: '명도이야기',
-      title: '나홀로 소송, 전체 프로세스 돌아보기',
-      desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
-    },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
+    // {
+    //   thumbnail:
+    //     'https://img.smlounge.co.kr/upload/woman/article/202207/thumb/51473-493033-sample.png',
+    //   link: 'https://post.naver.com/viewer/postView.naver?volumeNo=34170287&memberNo=6345811',
+    //   category: '명도이야기',
+    //   title: '나홀로 소송, 전체 프로세스 돌아보기',
+    //   desc: "민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트 민사 소송에서 변호사 도움 없이 소송을 진행하는 '나홀로 소송'의 비율이 늘어나고 있다. '민사본안 소송 대리인 선임 현황 테스트",
+    // },
   ],
 };
 
