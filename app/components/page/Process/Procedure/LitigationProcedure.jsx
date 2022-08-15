44eb1bc10b4845b01e6a5b6539585a29342f41dc
kich555 <hyunjjang199582@gmail.com>
2022-08-15 16:06:49 +0900

    Refactoring LitigationProcedure section

--- app/components/page/Process/Procedure/LitigationProcedure.jsx
+++ app/components/page/Process/Procedure/LitigationProcedure.jsx
@@ -1,4 +1,4 @@
-import { Container, Center, Space, MediaQuery } from '@mantine/core';
+import { Container, Center, Space, MediaQuery, Group } from '@mantine/core';
 import useResponsive from '~/hooks/useResponsive';
 import BranchArrow from '~/asset/icon/procedure/BranchArrow';
 import useProcedureStyles from '~/Style/page/Process/useProcedureStyles';
@@ -9,29 +9,32 @@ import ProcedureList from '~/components/page/Process/Procedure/Desktop/Procedure
 
 export default function LitigationProcedure() {
   const { classes } = useProcedureStyles();
-  const { wrapper } = classes;
+  const { wrapper, container } = classes;
   const { PROCEDURE } = PROCESS_CONSTANTS;
   const { CONSULTATION } = PROCEDURE;
   const { mobile } = useResponsive();
   return (
-    <Container size='md' px={0} className={wrapper}>
-      <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
-        <div>
-          <Space h={80} />
-          <Container size='md' px={0}>
+    <section className={wrapper}>
+      <Container size='md' px={0} className={container}>
+        <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
+          <div>
+            <Space h={80} />
+            <Container size='md' px={0}>
+              <Center>
+                <Consultation procedureForm={CONSULTATION} />
+              </Center>
+            </Container>
             <Center>
-              <Consultation procedureForm={CONSULTATION} />
+              <BranchArrow />
             </Center>
-          </Container>
-          <Center>
-            <BranchArrow />
-          </Center>
-          <Space h={50} />
-        </div>
-      </MediaQuery>
-      <Container size='md' px={0}>
-        {mobile ? <MobileProcedureList /> : <ProcedureList />}
+            <Space h={50} />
+          </div>
+        </MediaQuery>
+        <Container size='md' px={mobile ? 26 : 0} style={{ width: '100%' }}>
+          {mobile ? <MobileProcedureList /> : <ProcedureList />}
+        </Container>
       </Container>
-    </Container>
+      <Space h={100} />
+    </section>
   );
 }
