cd243e703b741b002aeec45835b5d8baa531887f
kich555 <hyunjjang199582@gmail.com>
2022-08-15 16:06:16 +0900

    Create MobileProcedure styles

--- app/Style/page/Process/Mobile/useMobileProcedureStyles.jsx
+++ app/Style/page/Process/Mobile/useMobileProcedureStyles.jsx
@@ -1,52 +1,43 @@
 import { createStyles } from '@mantine/core';
 
 const useMobileProcedureStyles = createStyles((theme) => ({
-  wrapper: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    position: 'relative',
-    width: '100%',
-    height: '1030px',
-    overflowY: 'hidden',
-    textAlign: 'center',
-  },
-  openedWrapper: {
-    height: '100%',
-  },
-
-  shadowButton: {
-    position: 'absolute',
-    bottom: 0,
-    transition: 'ease-in-out 1s',
-    '&::before': {
-      content: '" "',
-      position: 'absolute',
-      top: '-56px',
-      border: 'none',
-      width: '100%',
-      height: '55px',
-      background:
-        'linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%)',
-      transform: 'rotate(-180deg)',
-    },
-  },
-
-  button: {
+  icon: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    height: '55px',
-    width: '100%',
-    border: '1px solid',
-    borderRadius: '3px',
-    borderColor: theme.black,
-    backgroundColor: theme.white,
-    fontSize: '18px',
-    fontWeight: 700,
+    width: '20px',
+    height: '20px',
+    borderRadius: '50%',
+    backgroundColor: theme.colors[theme.primaryColor],
   },
-  maxWidth: {
+
+  labelTitle: {
+    display: 'flex',
+    fontSize: '20px',
+  },
+  labelDesc: {
+    lineHeight: '150%',
+  },
+
+  chevron: {
+    alignSelf: 'start',
+    '&[data-rotate]': {
+      transform: 'rotate(90deg)',
+    },
+  },
+  item: {
     width: '100%',
+    borderBottom: '1px solid #EEEEEE',
+    '&:nth-of-type(1)': {
+      borderTop: '1px solid #EEEEEE',
+    },
+  },
+
+  control: {
+    padding: '20px 0',
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
   },
 }));
 
