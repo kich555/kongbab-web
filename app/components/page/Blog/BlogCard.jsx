f81e884f4a501547de24e80731a02696a2a9139e
kich555 <hyunjjang199582@gmail.com>
2022-08-20 23:43:45 +0900

    Refactoring BlogCard component

--- app/components/page/Blog/BlogCard.jsx
+++ app/components/page/Blog/BlogCard.jsx
@@ -1,18 +1,10 @@
-import { Card, Image, Text } from '@mantine/core';
+import { Card, Image, Space, Text } from '@mantine/core';
 import useResponsive from '~/hooks/useResponsive';
 import useBlogCardStyles from '~/Style/page/Blog/useBlogCardStyles';
 
 export default function BlogCard({ imageSrc, link, category, title, desc }) {
-const { classes, theme } = useBlogCardStyles();
-  const {
-    card,
-    cardSection,
-    image,
-
-    titleText,
-    descText,
-    content,
-  } = classes;
+  const { classes, theme } = useBlogCardStyles();
+  const { card, cardSection, image, descText } = classes;
   const { colors, fn } = theme;
   const linkProps = {
     href: link,
@@ -23,30 +15,30 @@ const { classes, theme } = useBlogCardStyles();
   return (
     <Card className={card}>
       <Card.Section className={cardSection}>
-        <a {...linkProps}>
-          <Image src={imageSrc} styles={{ image }} width={mobile ? 324 : 344} />
-        </a>
+        {/* <a {...linkProps}> */}
+        <Image
+          alt={`thumbnail-${title}`}
+          src={imageSrc}
+          styles={{ image }}
+          width={mobile ? 322 : 344}
+        />
+        {/* </a> */}
       </Card.Section>
-      <div className={content}>
+      <div>
+        <Space h={18} />
         <Text color={colors[theme.primaryColor]} size='sm'>
           {category}
         </Text>
-        <div className={titleText}>
-          <Text
-            className={titleText}
-            {...linkProps}
-            component='a'
-            weight={500}
-            lineClamp={2}
-          >
-            {title}
-          </Text>
-        </div>
+        <Space h={mobile ? 8 : 12} />
+        <Text size={20} {...linkProps} component='a' weight={500} lineClamp={2}>
+          {title}
+        </Text>
+        {!mobile && <Space h={18} />}
         <Text
-          className={descText}
           size='sm'
           color={colors.gray[3]}
           lineClamp={2}
+          className={descText}
         >
           {desc}
         </Text>
