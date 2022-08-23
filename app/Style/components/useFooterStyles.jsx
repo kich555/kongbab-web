5defb4f5ca115b9523b74341a4f43762d6f722ca
kich555 <hyunjjang199582@gmail.com>
2022-08-23 20:51:34 +0900

    Refactoring Footer styles

--- app/Style/components/useFooterStyles.jsx
+++ app/Style/components/useFooterStyles.jsx
@@ -1,7 +1,7 @@
 import { createStyles } from '@mantine/core';
 
 const useFooterStyles = createStyles((theme) => ({
-  footer: {
+  wrapper: {
     display: 'flex',
     alignItems: 'end',
     minHeight: '240px',
@@ -10,7 +10,8 @@ const useFooterStyles = createStyles((theme) => ({
     borderTop: '1px solid #0d0d0d',
     backgroundColor: theme.black,
   },
-  container: {
+
+  inner: {
     display: 'flex',
     justifyContent: 'space-between',
     width: '100%',
@@ -23,48 +24,14 @@ const useFooterStyles = createStyles((theme) => ({
   },
 
   links: {
-    // display: 'flex',
-    // flexDirection: 'column',
-    // alignItems: 'end',
-    // justifyContent: 'end',
     [theme.fn.smallerThan('sm')]: {
       alignItems: 'start',
       justifyContent: 'start',
     },
   },
+
   alignSelfStart: {
     alignSelf: 'start',
-  },
-  flexRow: {
-    display: 'flex',
-  },
-  menuItem: {
-    fontWeight: 500,
-    '&:nth-of-type(2)': {
-      '&::before': {
-        content: "'|'",
-        paddingRight: '6px',
-        paddingLeft: '6px',
-      },
-    },
-  },
-  lineHeight150: { lineHeight: '150%' },
-  wrapReverse: {
-    flexWrap: 'wrapReverse',
-  },
-  gray: {
-    color: '#cccccc',
-  },
-
-  link: {
-    color: '#CCCCCC',
-    fontWeight: 500,
-  },
-
-  linkButton: {
-    padding: 0,
-    height: '40px',
-    border: 'none',
   },
 }));
 
