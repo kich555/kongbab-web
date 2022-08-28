92cdcf9b6e2c8be3481ddf78975cd93fcba51c46
kich555 <hyunjjang199582@gmail.com>
2022-08-28 23:38:59 +0900

    add Other page's sitemap

--- app/routes/[sitemap.xml].jsx
+++ app/routes/[sitemap.xml].jsx
@@ -1,12 +1,37 @@
 export const loader = () => {
   // separating xml content from Response to keep clean code.
   const content = `
-          <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-      <loc>https://congbab.com/</loc>
-      <lastmod>2022-08-15T00:15:16+01:00</lastmod>
-      <priority>1.0</priority>
-      </url>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+          <loc>https://congbab.com/</loc>
+          <lastmod>2022-08-15T00:15:16+01:00</lastmod>
+          <priority>1.0</priority>
+        </url>
+        <url>
+          <loc>https://congbab.com/md/introduce</loc>
+          <lastmod>2022-08-28T14:35:25+00:00</lastmod>
+          <priority>0.80</priority>
+        </url>
+        <url>
+          <loc>https://congbab.com/md/process</loc>
+          <lastmod>2022-08-28T14:35:25+00:00</lastmod>
+          <priority>0.80</priority>
+        </url>
+        <url>
+          <loc>https://congbab.com/md/pricing</loc>
+          <lastmod>2022-08-28T14:35:25+00:00</lastmod>
+          <priority>0.80</priority>
+        </url>
+        <url>
+          <loc>https://congbab.com/md/faq</loc>
+          <lastmod>2022-08-28T14:35:25+00:00</lastmod>
+          <priority>0.80</priority>
+        </url>
+        <url>
+          <loc>https://congbab.com/md/blog</loc>
+          <lastmod>2022-08-28T14:35:25+00:00</lastmod>
+          <priority>0.80</priority>
+        </url>
       </urlset>
       `;
   // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
