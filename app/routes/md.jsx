4926e2e681150e8d70cab7e0c1150cc99f987a45
kich555 <hyunjjang199582@gmail.com>
2022-08-28 23:44:07 +0900

    Add canonical on /md pages <header>

--- app/routes/md.jsx
+++ app/routes/md.jsx
@@ -1,9 +1,25 @@
 import { redirect } from '@remix-run/node';
+import { Links } from '@remix-run/react';
 
 export async function loader() {
   return redirect('/');
 }
 
+export function links() {
+  return [
+    {
+      rel: 'canonical',
+      href: 'https://congbab.com/',
+    },
+  ];
+}
+
 export default function md() {
-  return;
+  return (
+    <html lang='ko'>
+      <head>
+        <Links />
+      </head>
+    </html>
+  );
 }
