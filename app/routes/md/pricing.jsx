6846d9e297b356568864d968414bfaa95bce5785
kich555 <hyunjjang199582@gmail.com>
2022-09-04 20:07:15 +0900

    Modify Margin

--- app/routes/md/pricing.jsx
+++ app/routes/md/pricing.jsx
@@ -5,11 +5,13 @@ import PRICING_CONSTANTS from '~/constants/page/pricingPageData';
 import FAQ_CONSTANTS from '~/constants/page/faqPageData';
 import PriceInfoListSection from '~/components/common/PriceInfo/PriceInfoListSection';
 import { Space, useMantineTheme } from '@mantine/core';
+import useResponsive from '~/hooks/useResponsive';
 
 export default function PricingRoute(params) {
   const { RATE_PLANS } = PRICING_CONSTANTS;
   const { PRICE_FAQ } = FAQ_CONSTANTS;
   const theme = useMantineTheme();
+  const { mobile } = useResponsive();
   return (
     <>
       <section style={{ backgroundColor: theme.colors.gray[0] }}>
@@ -17,7 +19,7 @@ export default function PricingRoute(params) {
         <FeatureList />
       </section>
       {/* <EventSection /> */}
-      <Space h={230} />
+      <Space h={mobile ? 100 : 150} />
       <FeaturedFAQSection questions={PRICE_FAQ} />
     </>
   );
