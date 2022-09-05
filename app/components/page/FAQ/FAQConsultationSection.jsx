3c0af1aa504f9692daf5e4e78b7b8e26c465243a
kich555 <hyunjjang199582@gmail.com>
2022-09-05 11:20:30 +0900

    Modify faq page layout

--- app/components/page/FAQ/FAQConsultationSection.jsx
+++ app/components/page/FAQ/FAQConsultationSection.jsx
@@ -7,16 +7,16 @@ export default function FAQConsultationSection() {
   const theme = useMantineTheme();
   const { white } = theme;
   return (
-    <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
-      <section>
-        <ConsultationCard asset={asset}>
-          <MultiLineText size={30} align='center' color={white} weight={700}>
-            아직 망설여지나요?
-            <br />
-            상담은 언제나 환영합니다.
-          </MultiLineText>
-        </ConsultationCard>
-      </section>
-    </MediaQuery>
+    // <MediaQuery smallerThan='sm' styles={{ display: 'none' }}>
+    <section>
+      <ConsultationCard asset={asset}>
+        <MultiLineText size={30} align='center' color={white} weight={700}>
+          아직 망설여지나요?
+          <br />
+          상담은 언제나 환영합니다.
+        </MultiLineText>
+      </ConsultationCard>
+    </section>
+    // </MediaQuery>
   );
 }
