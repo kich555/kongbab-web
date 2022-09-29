6e712a1752eed9bbb588113c0b65d32e7d45c492
kich555 <hyunjjang199582@gmail.com>
2022-09-30 06:03:14 +0900

    Seperate ConsultationForm & FormBox on index/home

--- app/components/page/Main/ConsultationForm.jsx
+++ app/components/page/Main/ConsultationForm.jsx
@@ -2,8 +2,8 @@ import { Container, Space, Text } from '@mantine/core';
 import useResponsive from '~/hooks/useResponsive';
 import Title from '~/components/common/Title';
 import useConsultationFormStyles from '~/Style/page/Main/useConsultationFormStyles';
-
-export default function ConsultationForm({ children }) {
+import FormBox from './FormBox';
+export default function ConsultationForm({ actionData, isPosting, isLoading }) {
   const { tablet } = useResponsive();
   const { classes, theme } = useConsultationFormStyles();
   const { lineHeight150 } = classes;
@@ -23,8 +23,11 @@ export default function ConsultationForm({ children }) {
           아래 내용을 남겨주시면 빠르게 연락드리겠습니다.
         </Text>
         <Space h={80} />
-        {/* <FormBox /> */}
-        {children}
+        <FormBox
+          actionData={actionData}
+          isPosting={isPosting}
+          isLoading={isLoading}
+        />
         <Space h={150} />
       </Container>
     </section>
