a6ac8f86418f8a41d8ab105433c0926dce6fa6bf
kich555 <hyunjjang199582@gmail.com>
2022-09-30 06:03:37 +0900

    Move API CAll Parts to Server & Refactoring Form Component

--- app/components/page/Main/FormBox.jsx
+++ app/components/page/Main/FormBox.jsx
@@ -1,71 +1,84 @@
 import { Form } from '@remix-run/react';
-import { useState, useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import {
-  Space,
-  Text,
-  FileButton,
-  Button,
-  Checkbox,
-  List,
-  Anchor,
-  Group,
-  Center,
-} from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { Space, Text, Button, Anchor } from '@mantine/core';
 import useFormBoxStyles from '~/Style/page/Main/useFormBoxStyles';
-import Garbage from '~/asset/icon/Garbage';
-import { redirect } from '@remix-run/node';
 
-export const action = async ({ request }) => {
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: '/md/home',
-    },
-  });
-};
+import { showNotification, updateNotification } from '@mantine/notifications';
 
-export default function FormBox() {
-  const form = useRef();
-  const [files, setFiles] = useState([]);
+export default function FormBox({ actionData, isPosting, isLoading }) {
   const [isChecked, setIsChecked] = useState(false);
   const { classes, theme, cx } = useFormBoxStyles();
-  const {
-    error,
-    inputWrapper,
-    input,
-    fileInput,
-    label,
-    textInput,
-    button,
-    submmit,
-  } = classes;
+  const { inputWrapper, input, label, textInput, anchor, errorBorder, button } =
+    classes;
   const { colors } = theme;
 
+  useEffect(() => {
+    if (isPosting) {
+      showNotification({
+        id: 'load-data',
+        loading: isPosting,
+        title: '상담신청 접수중..',
+        message: '현재 상담신청을 접수중에 있습니다.',
+        autoClose: false,
+        disallowClose: true,
+      });
+    }
+    if (!actionData?.fieldErrors && isLoading) {
+      updateNotification({
+        id: 'load-data',
+        color: 'teal',
+        title: '접수 완료',
+        message: '상담신청이 접수되었습니다.',
+        autoClose: 2000,
+      });
+    }
+    if (actionData?.fieldErrors) {
+      updateNotification({
+        id: 'load-data',
+        color: 'red',
+        title: '접수 오류',
+        message: '접수가 반려되었습니다.',
+        autoClose: 2000,
+      });
+    }
+  }, [isPosting, isLoading, actionData]);
+
   return (
-    // <Form method='post' ref={form} key='consult'>
-    <Form action='/md/home' method='post'>
-      <label>
+    <Form method='post'>
+      <label htmlFor='name-input'>
         <Text size='md' weight={500}>
           이름
         </Text>
         <Space h={10} />
-        <div className={inputWrapper}>
+        <div
+          className={cx(inputWrapper, {
+            [errorBorder]: actionData?.fieldErrors?.name,
+          })}
+        >
           <input
+            id='name-input'
             type='text'
             name='name'
-            placeholder='김임차'
+            placeholder='이름을 입력해 주세요'
+            defaultValue={actionData?.fields?.name}
+            aria-label='name'
+            aria-invalid={Boolean(actionData?.fieldErrors?.name)}
             className={textInput}
           />
         </div>
-        {/* {errors?.slug ? (
-        <em className='text-red-600'>{errors.slug}</em>
-      ) : null} */}
+        {actionData?.fieldErrors?.name && (
+          <>
+            <Space h={10} />
+            <Text size='sm' color={colors.red[0]} component='em'>
+              {actionData?.fieldErrors?.name}
+            </Text>
+          </>
+        )}
       </label>
 
       <Space h={35} />
 
-      <label>
+      <label htmlFor='phoneNumber-input'>
         <Text size='md' weight={500} component='div'>
           휴대폰 번호{' '}
           <Text size='md' color={colors.gray[3]} weight={500} component='span'>
@@ -73,85 +86,71 @@ export default function FormBox() {
           </Text>
         </Text>
         <Space h={10} />
-        <div className={inputWrapper}>
+        <div
+          className={cx(inputWrapper, {
+            [errorBorder]: actionData?.fieldErrors?.phoneNumber,
+          })}
+        >
           <input
+            id='phoneNumber-input'
             type='number'
             name='phoneNumber'
             placeholder='01012345678'
+            defaultValue={actionData?.fields?.phoneNumber}
+            aria-label='phoneNumber'
+            aria-invalid={Boolean(actionData?.fieldErrors?.phoneNumber)}
             className={textInput}
           />
         </div>
-        {/* {errors?.slug ? (
-        <em className='text-red-600'>{errors.slug}</em>
-      ) : null} */}
-      </label>
-
-      <Space h={35} />
-
-      <label>
-        <Text size='md' weight={500} component='div'>
-          임대차 계약서{' '}
-          <Text size='md' color={colors.gray[3]} weight={500} component='span'>
-            (선택)
-          </Text>
-        </Text>
-        <Space h={10} />
-        <FileButton
-          // name='files'
-          onChange={setFiles}
-          accept='image/png,image/jpeg'
-          multiple
-        >
-          {(props) => (
-            <Button radius='md' {...props} className={button}>
-              파일 첨부
-            </Button>
-          )}
-        </FileButton>
-        {/* {errors?.slug ? (
-        <em className='text-red-600'>{errors.slug}</em>
-      ) : null} */}
-      </label>
-
-      <List size='sm' mt={5}>
-        <Group spacing={10}>
-          {files.map((file, index) => (
-            <List.Item key={index}>
-              <div className={cx(inputWrapper, fileInput)}>
-                <input className={textInput} value={file.name} disabled />
-                <Garbage style={{ marginRight: '16px' }} />
-              </div>
-            </List.Item>
-          ))}
-        </Group>
-      </List>
-
-      <Space h={35} />
-      <Checkbox
-        onChange={(e) => setIsChecked(e.currentTarget.checked)}
-        checked={isChecked}
-        value={isChecked}
-        label={
+        {actionData?.fieldErrors?.phoneNumber && (
           <>
-            (필수){' '}
-            <Anchor
-              size='md'
-              href='https://mantine.dev'
-              target='_blank'
-              color='#3787E4'
-            >
-              개인정보 처리방침
-            </Anchor>{' '}
-            동의
+            <Space h={10} />
+            <Text size='sm' color={colors.red[0]} component='em'>
+              {actionData?.fieldErrors?.phoneNumber}
+            </Text>
           </>
-        }
-        size='md'
-        color='black'
-        classNames={{ input, label }}
-      />
-      <Space h={42} />
-      <Button type='submit' radius='md' className={submmit}>
-        카톡 상담신청
+        )}
+      </label>
+      <Space h={50} />
+      <label htmlFor='checkbox-input' className={label}>
+        <input
+          id='checkbox-input'
+          type='checkbox'
+          name='isAgree'
+          value={isChecked}
+          aria-label='agree-checkbox'
+          aria-invalid={isChecked}
+          className={input}
+          onChange={() => setIsChecked((checked) => !checked)}
+        />
+        <span>
+          (필수){' '}
+          <Anchor
+            size='md'
+            href='https://congbab.notion.site/2022-08-01-6ba190142b8845ec9cdcc2e290941e4b'
+            target='_blank'
+            className={anchor}
+            rel='noreferrer'
+          >
+            개인정보 처리방침
+          </Anchor>{' '}
+          동의
+        </span>
+      </label>
+
+      <Space h={30} />
+      <Button
+        type='submit'
+        radius='md'
+        loading={isPosting}
+        disabled={isPosting || !isChecked}
+        className={button}
+        sx={{
+          '&[data-disabled]': { opacity: 0.4, cursor: 'notAllowed' },
+          '&[data-loading]': { backgroundColor: 'red', cursor: 'notAllowed' },
+        }}
+      >
+        {isPosting ? '이메일 전송 중..' : '카톡 상담신청'}
       </Button>
     </Form>
   );
